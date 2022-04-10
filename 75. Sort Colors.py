95f28d153ec5dde28e69007f1c9b58e469c86b3c
Bernabas Mulugeta <mbernabas1@gmail.com>
2022-04-10 10:37:55 +0300

    easy

---
+++ 75. Sort Colors.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)):
+            m=nums[i]
+            for j in range(i,len(nums)):
+                if nums[j]<=m:
+                    m=nums[j]
+                    idx=j
+            nums[idx]=nums[i]
+            nums[i]=m
