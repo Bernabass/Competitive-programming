eb515a19c4720016c9d64349f9cb1c47806bbb29
Bernabas Mulugeta <mbernabas1@gmail.com>
2022-05-06 20:46:47 +0300

    medium

---
+++ 1968. Array With Elements Not Equal to Average of Neighbors.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def rearrangeArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums.sort()
+        for i in range(0,len(nums),2):
+            if i+1<=len(nums)-1:
+                tar=nums[i]
+                nums[i]=nums[i+1]
+                nums[i+1]=tar
+        return (nums)
