037bd8934e68c88104aa371b6bcc8b40ef9cf062
Bernabass <mbernabas1@gmail.com>
2022-08-18 16:40:24 +0300

    medium

--- temporary.py
+++ temporary.py
@@ -1,27 +1,17 @@
-import math
-nums = [44,22,33,11,1]
-threshold = 5
-test_cases=list(range(1,max(nums)+1))
-low=0
-high=len(test_cases) - 1
-while low<=high:
-    mid=(low+ high)//2
-    result=0
-    for i in nums:
-        result+=math.ceil(i/(test_cases[mid]))
-    if low==high:
-        print(test_cases[mid])
-        break
-    if result > threshold:
-        low=mid + 1
-    else:
-        if mid-1 < 0:
-            print(test_cases[mid])
-            break
-        result=0
-        for i in nums:
-            result+=math.ceil(i/(test_cases[mid-1]))
-        if result>threshold:
-            print(print(test_cases[mid]))
-            break
-        high = mid-1
+from collections import defaultdict
+
+
+# Function to return a default
+# values for keys that is not
+# present
+def def_value():
+	return "Not Present"
+	
+# Defining the dict
+d = defaultdict(def_value)
+d["a"] = 1
+d["b"] = 2
+
+print(d["a"])
+print(d["b"])
+print(d["c"])
