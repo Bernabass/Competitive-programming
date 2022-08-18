b79943c6b63ffbdc1572988a32d8f10592827639
Bernabass <mbernabas1@gmail.com>
2022-08-18 12:47:03 +0300

    medium

--- temporary.py
+++ temporary.py
@@ -1,33 +1,21 @@
-arr=[5,7,7,8,8,10]
-target=13
-def find(A, target,position):
-    low = 0
-    high = len(A) - 1
-    while low <= high:
-        mid = (low+ high) // 2
-        if A[mid] < target:
-            low = mid + 1
-        elif A[mid] > target:
-            high = mid-1
-        else:
-            if position=='left':
-                if mid - 1 < 0:
-                    return mid
-                if A[mid-1] != target:
-                    return mid
-                high = mid - 1
-            elif position=='right':
-                if mid + 1 >=len(A):
-                    return mid
-                if A[mid+1] != target:
-                    return mid
-                low = mid+1
-    return None
-first=find(arr, target,"left")
-last=find(arr, target,"right")
-if first==None:
-    print([-1,-1])
-elif first==last:
-    print([first,first])
+citations = [0,1,3,5,6]
+low=0
+high = len(citations) - 1
+while low <= high:
+    mid = (low+ high) // 2
+    if low==high and citations[mid] >=len(citations)-mid:
+        print(len(citations)-mid)
+        break    
+    if citations[mid] >=len(citations)-mid:
+        if mid - 1 < 0:
+            print(len(citations)-mid)
+            break
+        if citations[mid-1] < (len(citations)-mid+1):
+            print(len(citations)-mid)
+            break
+        high = mid - 1      
+    else:
+        low = mid+1
 else:
-    print([first,last])
+    print(0)
+    
