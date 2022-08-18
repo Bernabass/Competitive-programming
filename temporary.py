9919108040658c13cf27b91cacdf201bf74c5d58
Bernabass <mbernabas1@gmail.com>
2022-08-18 14:53:16 +0300

    medium

--- temporary.py
+++ temporary.py
@@ -1,21 +1,27 @@
-citations = [0,1,3,5,6]
+import math
+nums = [44,22,33,11,1]
+threshold = 5
+test_cases=list(range(1,max(nums)+1))
 low=0
-high = len(citations) - 1
-while low <= high:
-    mid = (low+ high) // 2
-    if low==high and citations[mid] >=len(citations)-mid:
-        print(len(citations)-mid)
-        break    
-    if citations[mid] >=len(citations)-mid:
-        if mid - 1 < 0:
-            print(len(citations)-mid)
-            break
-        if citations[mid-1] < (len(citations)-mid+1):
-            print(len(citations)-mid)
-            break
-        high = mid - 1      
+high=len(test_cases) - 1
+while low<=high:
+    mid=(low+ high)//2
+    result=0
+    for i in nums:
+        result+=math.ceil(i/(test_cases[mid]))
+    if low==high:
+        print(test_cases[mid])
+        break
+    if result > threshold:
+        low=mid + 1
     else:
-        low = mid+1
-else:
-    print(0)
-    
+        if mid-1 < 0:
+            print(test_cases[mid])
+            break
+        result=0
+        for i in nums:
+            result+=math.ceil(i/(test_cases[mid-1]))
+        if result>threshold:
+            print(print(test_cases[mid]))
+            break
+        high = mid-1
