8dd352826dad4ddfe96d21604ea53014a38d1a90
Bernabass <mbernabas1@gmail.com>
2022-12-02 22:35:44 +0300

    easy

--- 733. Flood Fill.py
+++ 733. Flood Fill.py
@@ -1,18 +1,16 @@
-image = [[1,1,1],[1,1,0],[1,0,1]]
-sr = 1
-sc = 1
-color = 2
-queue = [(sr,sc)]
-image[sr][sc] = color
-visited = set([sr,sc])
-while queue:
-    p = queue.pop()
-    print(p)
-    for x,y in [[0,1],[1,0],[0,-1],[-1,0]]:
-        temp = (p[0]+x,p[1]+y)
-        print(temp)
-        if temp not in visited:
-            queue.append(temp)
-            visited.add(temp)
-            image[p[0]+x][p[1]+y] = color
-print(image)
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        queue = [[sr,sc]]
+        starting_pixel = image[sr][sc]
+        image[sr][sc] = color
+        visited = set([sr,sc])
+        while queue:
+            p = queue.pop()
+            for x,y in [[0,1],[1,0],[0,-1],[-1,0]]:
+                if 0 <= p[0] + x < len(image) and 0 <= p[1] + y < len(image[0]):
+                    neighbour = (p[0]+x,p[1]+y)
+                    if neighbour not in visited and image[neighbour[0]][neighbour[1]] == starting_pixel:
+                        queue.append(neighbour)
+                        visited.add(neighbour)
+                        image[neighbour[0]][neighbour[1]] = color
+        return image
