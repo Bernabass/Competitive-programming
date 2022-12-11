f521ddfcaef9d9938bfda8a9f3df5dc19ca9dc4f
Bernabass <99589215+Bernabass@users.noreply.github.com>
2022-12-11 22:38:55 +0300

    Time: 348 ms (85.92%), Space: 21.8 MB (31.42%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,27 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows = len(grid) - 1
-        colums = len(grid[0]) - 1
-        ones = set()
+        m = len(grid)
+        n = len(grid[0])
         neighbours = [[1,0],[0,1],[-1,0],[0,-1]]
-        for r in range(rows + 1):
-            for c in range(colums + 1):
-                if grid[r][c] == '1':
-                    ones.add(((r,c)))
-        res = len(ones)
-        while ones:
-            visited = set()
-            node = ones.pop()
+        visited = set()
+        count = 0
+        def bfs(node):
             visited.add((node))
             node = [node]
             while node:
                 new = []
                 for i,j in node:
                     for x,y in neighbours:
-                        if (i+x,j+y) in ones and (i+x,j+y) not in visited:
+                        if 0 <= i+x < m and 0 <= j+y < n and grid[i+x][j+y] == '1' and (i+x,j+y) not in visited:
                             new.append((i+x,j+y))
                             visited.add(((i+x,j+y)))
                 node = new
-            ones -= visited 
-            res = res - len(visited) + 1
-        return res
+            return 
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == '1' and (r,c) not in visited:
+                    bfs((r,c))
+                    count += 1
+        return count
