3e9341cfd233692abfa8d7cb2464bc86f0e30a75
Bernabass <99589215+Bernabass@users.noreply.github.com>
2022-12-13 00:00:01 +0300

    Time: 53 ms (55.60%), Space: 13.9 MB (61.88%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -1,8 +1,4 @@
 class Solution:
     def numTrees(self, n: int) -> int:
         name = [1,2,5,14,42,132,429,1430,4862,16796,58786,208012,742900,2674440,9694845,35357670,129644790,477638700,1767263190]
-        key = []
-        for i in range(1,20):
-            key.append(i)
-        sol = dict(zip(key,name))
-        return sol[n]
+        return name[n-1]
