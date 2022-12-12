d32638ac91c71304b347b7a89e02e26bae356bb6
Bernabass <99589215+Bernabass@users.noreply.github.com>
2022-12-12 14:30:06 +0300

    Time: 59 ms (31.61%), Space: 13.8 MB (58.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,22 +1,14 @@
 class Solution:
     def climbStairs(self, n: int) -> int   : 
-        root = 0
-        stack = []
-        best = {}
+        stack, best = -1 , {}
         while 1:
-            if root  <= n:
-                stack.append(root)
-                root = root + 1
+            if stack + 1  <= n:stack += 1
             else:
-                p = stack.pop()
-                if p + 1 > n:
-                    best[p] = 1
-                    while stack:
-                        root = stack.pop()
-                        temp = 0
-                        if root + 1 <= n :
-                            temp += best[root + 1]
-                        if root + 2 <= n:
-                            temp += best[root + 2]
-                        best[root] = temp
-                    return best[0]
+                best[stack] = 1
+                while stack >= 0:
+                    stack -= 1
+                    temp = 0
+                    if stack + 1 <= n :temp += best[stack + 1]
+                    if stack + 2 <= n:temp += best[stack + 2]
+                    best[stack] = temp
+                return best[0]
