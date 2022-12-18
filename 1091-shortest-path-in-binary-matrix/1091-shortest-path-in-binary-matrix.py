2d029a77d35a9d25f1d550bd3b1264ffb74c1ef6
Bernabass <99589215+Bernabass@users.noreply.github.com>
2022-12-18 14:56:10 +0300

    Time: 4741 ms (5.03%), Space: 16.8 MB (5.13%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -5,25 +5,45 @@ class Solution:
             return -1
         if len(grid[0]) == 1:
             return 1
-        neigbours = [[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]]
-        level = [[0,0]]
-        memory = {}
-        for i in range(n):
-            for j in range(n):
-                if grid[i][j] == 0:
-                    memory[(i,j)] = 1
-        count = 1
-        visited = {(0,0)}
-        while level:
-            levelX = []
+        adj = [[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]]
+        def bfs(node):
+            result = set()
+            level = [node]
+            visited = {(node[0],node[1])}
             for i , j in level:
-                for r , c in neigbours:
+                for r , c in adj:
                     if 0 <= i+r < n and 0 <= j+c < n and grid[i+r][j+c] == 0 and (i+r,j+c) not in visited:
-                        memory[(i+r,j+c)] += count
-                        if (i+r,j+c) == (n-1,n-1):
-                            return memory[(i+r,j+c)]
                         visited.add((i+r,j+c))
-                        levelX.append([i+r,j+c])
-            count += 1
-            level = levelX
+                        result.add((i+r,j+c))
+            return result
+        front = {(0,0)}
+        back = {(n-1,n-1)}
+        count_f = 1
+        count_b = 1
+        seen = set()
+        flag = False
+        while not flag:
+            flag = True
+            org = front.copy()
+            count_org = count_f
+            for x,y in front:
+                if (x,y) not in seen:
+                    flag = False
+                    seen.add((x,y))
+                    ans = bfs((x,y))
+                    front = front.union(ans)
+            count_f += 1
+            if front & back:
+                return count_f + count_b - 1
+            for a,b in back:
+                if (a,b) not in seen:
+                    flag = False
+                    seen.add((a,b))
+                    ans = bfs((a,b))
+                    back = back.union(ans)
+            count_b += 1
+            if org & back:
+                return count_org + count_b - 1
+            if front & back:
+                return count_f + count_b - 1
         return -1
