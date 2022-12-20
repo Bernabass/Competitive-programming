151f1c2676ff16481ed59523ebbe71d7e49b31ef
Bernabass <99589215+Bernabass@users.noreply.github.com>
2022-12-20 17:52:42 +0300

    Time: 43 ms (86.15%), Space: 13.9 MB (29.11%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,4 +1,18 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        return str(eval(num1+"*"+num2))
+        n1 , n2 = len(num1) , len(num2)
+        count1 , count2 = 0 , 0
+        res1 , res2 = 0 , 0
+        for i in range(n1-1,-1,-1):
+            digit1 = 10**(i)
+            temp = ord(num1[count1]) - 48
+            res1 += temp * digit1
+            count1 += 1
+        for j in range(n2-1,-1,-1):
+            digit2 = 10**(j)
+            temp = ord(num2[count2]) - 48
+            res2 += temp * digit2
+            count2 += 1
+        return str(res1 * res2)
+    
         
