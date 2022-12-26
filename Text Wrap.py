53bf2bdbb4b50edbb0afa1143f11c40385c1d0ea
Bernabass <mbernabas1@gmail.com>
2022-12-26 14:48:04 +0300

    easy

---
+++ Text Wrap.py
@@ -0,0 +1,18 @@
+import textwrap
+
+def wrap(string, max_width):
+    count = 0
+    paragraph = ""
+    res = ""
+    for idx , char in enumerate(string):
+        count += 1
+        paragraph += char 
+        if count == max_width or idx == len(string) - 1 :
+            res += paragraph + "\n"
+            paragraph = ""
+            count = 0   
+    return res
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
