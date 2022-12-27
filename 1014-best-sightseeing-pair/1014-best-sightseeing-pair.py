b7c3679167d6fb9db46d0f443ccf19aba00770c0
Bernabass <99589215+Bernabass@users.noreply.github.com>
2022-12-27 15:38:45 +0300

    Time: 506 ms (90.61%), Space: 20.6 MB (32.27%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
@@ -3,12 +3,10 @@ class Solution:
         best = values[1] + values[0] - 1
         maxx = 0
         for i in range(1,len(values)):
-            current = values[i] + values[i-1] - 1
+            # current = values[i] + values[i-1] - 1
             temp = values[i] + values[maxx] + (maxx - i)
-            if temp > current:
-                best = max(temp,best)
-            else:
-                best = max(best,current)
+            best = max(temp,best)
+            # best = max(best,current)
             if values[i] + i - maxx >= values[maxx]:
                 maxx = i
         return best
