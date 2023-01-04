c461fa9151a87b9012545b3d4282a36db2c3a43b
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-01-04 15:04:02 +0300

    Time: 38 ms (67.28%), Space: 13.9 MB (13.78%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.py
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.py
@@ -1,17 +1,26 @@
 class Solution:
     def divisorSubstrings(self, num: int, k: int) -> int:
+        '''
+        The problem is solved using 
+        the fixed sliding window approach.
+        And to reduce time complexity "deque" is
+        used instead of string concatination or list.
+        '''
         string = str(num)
         substring, n = deque(), len(string)
         k_beauty = left = right = 0
         
+        # Preparing the fixed window.
         for idx in range(k):
             substring.append(string[idx])
             right += 1
-        
+    
+        # Checking if there is a k-beauty in the prepared window.
         cur_num = int("".join(substring))
         if cur_num and num % cur_num  == 0:
             k_beauty += 1
         
+        # Counting the number of k-beauty by sliding the window
         while right < n:
             substring.popleft()
             substring.append(string[right])
