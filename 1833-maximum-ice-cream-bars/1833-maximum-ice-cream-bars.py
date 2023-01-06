14bcd79bcda886d13e3b678919f6b4064ff29b3d
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-01-06 08:54:28 +0300

    Time: 855 ms (88.48%), Space: 27.9 MB (61.26%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -1,10 +1,20 @@
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
-        costs.sort()
-        bars = total_cost = 0
-        for price in costs:
-            if total_cost + price > coins:
-                return bars
-            total_cost += price
-            bars += 1
-        return bars
+        counted = [0] * (max(costs)+ 1)
+        res = 0
+        for idx in costs:
+            counted[idx] += 1
+        counted = counted[1:]
+        
+        for val, freq in enumerate(counted,1):
+            if val > coins:
+                return res
+            max_ = coins // val
+            if max_ >= freq:
+                res += freq
+                coins -= val * freq
+            else:
+                res += max_
+                coins -= max_ * val
+        return res
+            
