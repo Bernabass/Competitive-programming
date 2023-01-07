22cb0c7ed596f01752f4cf5f92359ebaaa4ed370
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-01-07 20:13:37 +0300

    Time: 33 ms (76.40%), Space: 14 MB (14.61%) - LeetHub

--- 0537-complex-number-multiplication/0537-complex-number-multiplication.py
+++ 0537-complex-number-multiplication/0537-complex-number-multiplication.py
@@ -1,10 +1,7 @@
 class Solution:
     def complexNumberMultiply(self, num1: str, num2: str) -> str:
-        num1 = num1.split("+")
-        num2 = num2.split("+")
-        first = [int(num1[0]) * int(num2[0]),int(num1[0]) * int(num2[1][:-1])]
-        second = [int(num1[1][:-1]) * int(num2[0]), -(int(num1[1][:-1]) * int(num2[1][:-1]))]
-        second.reverse()
-        res = [first[0] + second[0], first[1] + second[1]]
-        return str(res[0]) + "+" + str(res[1]) + "i"
-                  
+        num1 = list(map(int, num1.rstrip('i').split('+')))
+        num2 =list(map(int, num2.rstrip('i').split('+')))
+        real = num1[0] * num2[0] - num1[1] * num2[1]
+        imag = num1[0] * num2[1] + num2[0] * num1[1]
+        return f"{real}+{imag}i"
