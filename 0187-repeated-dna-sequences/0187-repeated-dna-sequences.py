76c95c9893e19f1c44c4bf9ca58c9c879b2a13a6
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-01-07 12:06:15 +0300

    Time: 60 ms (94.28%), Space: 26 MB (70.72%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -1,19 +1,12 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         n = len(s)
-        ans = []
-        sequence = s[:10]
-        repeated = {sequence:1}
-        for idx in range(10,n):
-            sequence = list(sequence)
-            sequence.pop(0)
-            sequence.append(s[idx])
-            sequence = "".join(sequence)
+        ans = set()
+        repeated = {s[:10]}
+        for idx in range(1,n - 9):
+            sequence = s[idx:idx+10]
             if sequence in repeated:
-                if repeated[sequence] > 0:
-                    ans.append(sequence)
-                    repeated[sequence] -= 1
-            else:
-                repeated[sequence] = 1
+                ans.add(sequence)
+            repeated.add(sequence)
         
-        return ans
+        return [*(ans)]
