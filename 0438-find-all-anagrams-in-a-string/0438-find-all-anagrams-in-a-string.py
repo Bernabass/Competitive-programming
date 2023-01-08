984b4397243855ca7d08e6fc43236f094ce82d51
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-01-08 22:08:34 +0300

    Time: 352 ms (43.69%), Space: 15.1 MB (80.04%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -2,22 +2,19 @@ class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         n = len(s)
         window_size = len(p)
-        if window_size > n:
-            return []
-        anagram = Counter(p)
-        window, res = defaultdict(int), []
-        for i in range(window_size):
-            window[s[i]] += 1
-            if window == anagram:
-                res.append(0)
+        res = []
+        anagram, window = Counter(p), Counter(s[:window_size])
+        if window == anagram:
+            res.append(0)
         
-    
-        for idx in range(window_size,n):
-            window[s[idx-window_size]] -= 1
-            if window[s[idx-window_size]] <= 0:
-                window.pop(s[idx-window_size])
-            window[s[idx]] += 1
+        for idx in range(window_size, n):
+            front, back = s[idx-window_size], s[idx]
+            window[front] -= 1
+            if window[front] <= 0:
+                window.pop(front)
+            window[back] += 1
+            
             if window == anagram:
-                res.append(idx-window_size+1)
+                res.append(idx - window_size + 1)
         
         return res
