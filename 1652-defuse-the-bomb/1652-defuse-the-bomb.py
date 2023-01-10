eda931629c03245b2735193d37ca5dba0793780a
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-01-10 15:29:23 +0300

    Time: 34 ms (96.97%), Space: 13.9 MB (72.22%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.py
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.py
@@ -2,9 +2,9 @@ class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
         org = len(code)
         if not k:
-            return [0]*len(code)
+            return [0]*org
         elif k < 0:
-            initial = len(code) + k
+            initial = org + k
         else:
             initial = 1
             
@@ -13,8 +13,9 @@ class Solution:
         window_size = abs(k)
         prev = sum(code[initial:initial+window_size])
         decrypted_code = [prev]
-
-        for idx in range(initial+window_size,org+initial+window_size-1):
+        start = initial+window_size
+    
+        for idx in range(start,org+start-1):
             curr = prev - code[idx-window_size] + code[idx]
             decrypted_code.append(curr)
             prev = curr
