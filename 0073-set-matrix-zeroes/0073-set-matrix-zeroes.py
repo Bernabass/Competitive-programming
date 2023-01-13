f650dc7b24c889f536d8457354aa849a7bc8b956
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-01-13 18:12:39 +0300

    Time: 145 ms (60.44%), Space: 14.7 MB (63.29%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -5,27 +5,29 @@ class Solution:
         """
         neighbours = [[0,1],[1,0],[-1,0],[0,-1]]
         m, n = len(matrix), len(matrix[0])
-        for x in range(m):
-            for y in range(n):
-                if not matrix[x][y]:
-                    matrix[x][y] = "jezba"
+        
         def bfs(cell):
             for i, j in [cell]:
                 for r, c in neighbours:
                     while 0 <= i+r < m and 0 <= j+c < n:
-                        if matrix[i+r][j+c] not in {0,"jezba"}:
+                        if matrix[i+r][j+c] not in {0,"target"}:
                             matrix[i+r][j+c] = 0
                         i += r
                         j += c
                     i, j = cell[0], cell[1]
+                    
+        for x in range(m):
+            for y in range(n):
+                if not matrix[x][y]:
+                    matrix[x][y] = "target"
+
         
         for row in range(m):
             for col in range(n):
-                if matrix[row][col] == "jezba":
+                if matrix[row][col] == "target":
                     bfs([row,col])
         
-        print(matrix)
         for x in range(m):
             for y in range(n):
-                if matrix[x][y] == "jezba":
+                if matrix[x][y] == "target":
                     matrix[x][y] = 0
