af887de87ae9cdc3a22e9fa16c686f2b5a947a25
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-01-17 13:48:11 +0300

    Time: 94 ms (59.68%), Space: 13.9 MB (72.68%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,14 +1,12 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        m, n = len(matrix), len(matrix[0])
-        diagonal = defaultdict(set)
-        
-        for row in range(m):
-            for col in range(n):
-                diagonal[row-col].add(matrix[row][col])
+        diagonals = {}
+    
+        for m, row in enumerate(matrix):
+            for n, element in enumerate(row):
+                if m-n not in diagonals:
+                    diagonals[m-n] = element
+                elif diagonals[m-n] != element:
+                    return False
                 
-        for val in diagonal.values():
-            if len(val) != 1:
-                return False
-        
-        return True
+        return True      
