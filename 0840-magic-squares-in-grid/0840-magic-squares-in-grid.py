7c324a072723119b3c65e6cbb5bae11ff60033af
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-01-19 14:08:54 +0300

    Time: 37 ms (89.26%), Space: 13.9 MB (47.11%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -1,17 +1,17 @@
 class Solution:
     def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        count = 0
-        def checker(i,j):
-            odds, evens = set(range(1,10,2)), set(range(0,10,2))
+        row, col = len(grid), len(grid[0])
+        odds, evens = set(range(1,10,2)), set(range(0,10,2))
+        magic_square = 0
+        def isMagicSquare(i,j):
             edges, corners = set([5]), set([0])
             test1 = test2  = 0
             
-            for r, c in [[-1,-1],[1,1],[1,-1],[-1,1]]:
-                corners.add(grid[i+r][j+c])
+            for a, b in [[-1,-1],[1,1],[1,-1],[-1,1]]:
+                corners.add(grid[i+a][j+b])
                 
-            for x, y in [[1,0],[0,1],[0,-1],[-1,0]]:
-                edges.add(grid[i+x][j+y])
+            for c, d in [[1,0],[0,1],[0,-1],[-1,0]]:
+                edges.add(grid[i+c][j+d])
                 
             for idx in range(3):
                 test1 += grid[i-1][j+idx-1]
@@ -22,9 +22,9 @@ class Solution:
             
             return False
             
-        for r in range(1,m-1):
-            for c in range(1,n-1):
-                if grid[r][c] == 5 and checker(r,c):
-                    count += 1
+        for r in range(1,row-1):
+            for c in range(1,col-1):
+                if grid[r][c] == 5 and isMagicSquare(r,c):
+                    magic_square += 1
                     
-        return count
+        return magic_square
