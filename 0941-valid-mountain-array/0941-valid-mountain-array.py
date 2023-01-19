d62aef45d393f4280e24fe1dca4855e69233aac2
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-01-19 15:42:06 +0300

    Time: 220 ms (58.98%), Space: 15.3 MB (65.28%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.py
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -1,15 +1,11 @@
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
-        n = len(arr)
-        if n < 3:
-            return False
-        
-        count = -1
+        n, mountain = len(arr), False
         for i in range(1,n-1):
-            if arr[i] > arr[i-1] and arr[i] > arr[i+1]:
-                count += 1
+            if arr[i-1] < arr[i] > arr[i+1]:
+                mountain = True
                 
-            elif arr[i] <= arr[i-1] and arr[i] <= arr[i+1]:
+            elif arr[i-1] >= arr[i] <= arr[i+1]:
                 return False
             
-        return not count
+        return mountain
