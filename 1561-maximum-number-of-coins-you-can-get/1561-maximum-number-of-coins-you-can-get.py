a6f349927ae9dc5975ecaa079ac4a69c93a50365
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-01-23 15:34:06 +0300

    Time: 1930 ms (6.06%), Space: 26.7 MB (26.16%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
@@ -1,16 +1,14 @@
 class Solution:
     def maxCoins(self, piles: List[int]) -> int:
         piles.sort(reverse = True)
-        updated = deque(piles)
         max_coin = 0
+        left = 1
+        right = len(piles) - 1
         
-        
-        
-        while updated:
-            alice = updated.popleft()
-            mine = updated.popleft()
-            bob = updated.pop()
+        while left <= right:
+            max_coin += piles[left]
             
-            max_coin += mine
-            
+            left += 2
+            right -= 1
+           
         return max_coin
