2ab340343272d7db23065d1e1a44526880394a84
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-01-24 19:07:28 +0300

    Time: 385 ms (37.87%), Space: 15 MB (99.97%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,14 +3,28 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        priority, n = deque(), len(nums)
+        N = len(nums)
+        left = right = 0
         
-        for idx in range(n):
-            if not nums[idx]:
-                priority.append(idx)
-            else:
-                if priority:
-                    new_idx = priority.popleft()
-                    nums[new_idx] = nums[idx]
-                    nums[idx] = 0
-                    priority.append(idx)
+        while right < N:
+            if nums[right]:
+                nums[right], nums[left] = nums[left], nums[right]
+                left += 1
+            right += 1
+        
+        
+        
+        
+        
+        
+#         priority, n = deque(), len(nums)
+        
+#         for idx in range(n):
+#             if not nums[idx]:
+#                 priority.append(idx)
+#             else:
+#                 if priority:
+#                     new_idx = priority.popleft()
+#                     nums[new_idx] = nums[idx]
+#                     nums[idx] = 0
+#                     priority.append(idx)
