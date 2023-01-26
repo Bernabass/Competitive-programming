a28515e4a5165dfa3d694e406560c0af9fafdca3
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-01-26 14:07:52 +0300

    Time: 209 ms (36.14%), Space: 14.1 MB (85.71%) - LeetHub

--- 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.py
+++ 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.py
@@ -2,7 +2,6 @@ class Solution:
     def largestMerge(self, word1: str, word2: str) -> str:
         word1, word2 = deque(word1), deque(word2)
         res = []
-        
         while word1 and word2:
             if word1 > word2:
                 res.append(word1.popleft())
