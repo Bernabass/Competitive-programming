a417825660263b02d6dc6e3058cf99986e3846b4
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-02-08 11:18:59 +0300

    Time: 48 ms (19.96%), Space: 13.9 MB (66.34%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.py
+++ 0273-integer-to-english-words/0273-integer-to-english-words.py
@@ -2,51 +2,45 @@ class Solution:
     def numberToWords(self, num: int) -> str:
         if not num:
             return "Zero"
-        num = str(num)
-        digit = len(num)
-        number = [num[max(0,i-3):i] for i in range(len(num),0,-3)]
-        number.reverse()
-        nums = list(range(1,20))
-        nums.extend(range(20,100,10))
-        nums.extend([100, 1000,1000000,1000000000])
-        toWord = []
         
+        number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,30,40,50,60,70,80,90,100,1000,1000000,1000000000]
+        number = list(map(str,number))
         words = "One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen Twenty Thirty Forty Fifty Sixty Seventy Eighty Ninety Hundred Thousand Million Billion"
+        convert = dict(zip(number,words.split()))
         
-        info = dict(zip(nums,words.split()))
-        n = len(number)
-
-        for idx, chunk in enumerate(number):
-            if int(chunk) not in info:
+        num = str(num)
+        num = [num[max(0,i-3):i] for i in range(len(num),0,-3)]
+        num.reverse()
+        n, toWord = len(num), []
+        
+        for chunk in num:
+            if chunk not in convert:
                 if len(chunk) == 2:
-                    flag = True
-                    toWord.append(info[int(chunk[0])*10])
-                    if toWord.append(info[int(chunk[1])]):
-                        toWord.append(info[int(chunk[1])])
+                    toWord.append(convert[str(int(chunk[0])*10)])
+                    if int(chunk[1]):
+                        toWord.append(convert[chunk[1]])
                     
-                elif int(chunk) != 0:
-            
-                    if int(chunk[0]) != 0:
-                        toWord.append(info[int(chunk[0])])
+                elif chunk:
+                    if int(chunk[0]):
+                        toWord.append(convert[chunk[0]])
                         toWord.append("Hundred")
-                    if int(chunk[1]) == 1:
-                        toWord.append(info[int(chunk[1:3])])
+                    if chunk[1] == "1":
+                        toWord.append(convert[chunk[1:3]])
                     
                     else:   
-                        if int(chunk[1]) != 0:
-                            toWord.append(info[int(chunk[1])*10])
+                        if int(chunk[1]):
+                            toWord.append(convert[str(int(chunk[1])*10)])
 
-                        if int(chunk[2])!= 0:
-                            toWord.append(info[int(chunk[2])])
-                   
-                    
+                        if int(chunk[2]):
+                            toWord.append(convert[chunk[2]])
+                       
             else:
-                if len(str(int(chunk))) > 2:
+                if len(chunk) > 2:
                     toWord.append("One")
-                toWord.append(info[int(chunk)])
+                toWord.append(convert[chunk])
             
             if n > 1 and int(chunk):
-                toWord.append(info[10**(3*(n - 1))])
+                toWord.append(convert[str(10**(3*(n - 1)))])
             
             n -= 1
                                   
