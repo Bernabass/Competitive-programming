2b0623cd88ae1992b197678d0a32a1e60f47985b
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-03-03 10:52:35 +0300

    Time: 47 ms (42.92%), Space: 13.8 MB (46.90%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -6,10 +6,7 @@ class Solution:
         while left <= right:
             mid = (left+right) // 2
             
-            if mid ** 2 == x:
-                return mid
-            
-            elif mid ** 2 < x:
+            if mid ** 2 <= x:
                 left = mid + 1
                 
             else:
