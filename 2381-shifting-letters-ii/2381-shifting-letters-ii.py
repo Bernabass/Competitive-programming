9ff78dcac877a80fb42ad91d1056626268b81768
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-03-04 19:44:28 +0300

    Time: 1308 ms (92.56%), Space: 39.6 MB (10.74%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.py
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -2,25 +2,19 @@ class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
         n = len(s)
         arr = list(map(ord, list(s)))
-        added = [0]*n
+        added = [0]*(n+1)
         
         for query in shifts:
             left, right = query[0], query[1]
             forward = query[2]
             
             if forward:
-                if 0 <= left < n:
-                    added[left] += 1
-                    
-                if right + 1 < n:
-                    added[right+1] -= 1
+                added[left] += 1  
+                added[right+1] -= 1
                     
             else:
-                if 0 <= left < n:
-                    added[left] += -1
-
-                if right + 1 < n:
-                    added[right+1] -= -1
+                added[left] += -1
+                added[right+1] -= -1
             
             
         for i in range(1, len(added)):
@@ -31,7 +25,4 @@ class Solution:
             order += added[idx]  
             arr[idx] = chr((order - 97 ) % 26 + 97)
             
-        return "".join(arr)
-                    
-                  
-                    
+        return "".join(arr)  
