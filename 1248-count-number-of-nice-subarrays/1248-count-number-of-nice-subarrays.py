44b2838260ecaf8998f9c81a15387131a9c8222f
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-03-05 10:15:01 +0300

    Time: 1028 ms (19.85%), Space: 24.3 MB (11.28%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,15 +1,29 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        prefix_odd = nice_subArrays = 0
-        prev_prefix_odds = defaultdict(int)
-        prev_prefix_odds[0] += 1
+        n = len(nums)
+        ptr = left = window_sum = count = 0
+        nums.append(1)
+        last_valid_even = defaultdict(int)
         
-        for num in nums:
-            prefix_odd += num % 2
-            if prefix_odd - k in prev_prefix_odds:
-                nice_subArrays += prev_prefix_odds[prefix_odd - k]
-                
-            prev_prefix_odds[prefix_odd] += 1
+        
+        for i in range(n):
+            if ptr < i:
+                ptr = i
+            while ptr < n and (not nums[ptr + 1] % 2):
+                ptr += 1
+
+            last_valid_even[i] += ptr
+           
+        for right in range(n):
+            if nums[right] % 2:
+                window_sum += 1
             
-        return nice_subArrays
-                                  
+            while window_sum >= k and left <= right:
+                if window_sum == k:
+                    count += last_valid_even[right] - right + 1
+                 
+                if nums[left] % 2:
+                    window_sum -= 1
+                left += 1   
+                
+        return count
