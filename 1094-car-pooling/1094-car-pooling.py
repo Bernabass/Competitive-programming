b29ab0f992b3b8fe6492988239af7182c175081a
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-03-05 13:40:17 +0300

    Time: 72 ms (80.41%), Space: 14.4 MB (32.40%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.py
@@ -0,0 +1,26 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        max_to = float("-inf")
+        for trip in trips:
+            max_to = max(max_to, trip[2])
+            
+        arr = [0]*(max_to + 1)
+        n = len(arr)
+        
+        for trip in trips:
+            left, right = trip[1], trip[2]
+            val = trip[0]
+            
+            arr[left] += val
+            if right  < n:
+                arr[right] -= val
+                       
+        curr_max_passenger = prefix_sum = 0
+        
+        for num in arr:
+            prefix_sum += num
+            curr_max_passenger = max(curr_max_passenger, prefix_sum)
+            
+        return curr_max_passenger <= capacity
+        
+        
