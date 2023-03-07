a51e8ab1185c265b2d232c3dfc8ca7c0118db71e
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-03-07 09:14:00 +0300

    Time: 1096 ms (29.16%), Space: 36.2 MB (52.49%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.py
@@ -0,0 +1,27 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        def time(s, v):
+            return (target - s) / v
+        
+        info = list(zip(position, speed))
+        info.sort()
+        durations = []
+        for data in info:
+            temp = time(data[0], data[1])
+  
+            durations.append(temp)
+          
+        for i in range(1, len(durations)):
+            if durations[i] > durations[i-1]:
+                durations[i-1] = durations[i]
+                
+        stack = []
+        for i in range(len(durations)-1, -1, -1):
+            while stack and durations[stack[-1]] > durations[i]:
+                durations[i] = durations[stack[-1]] 
+                
+            stack.append(i)
+          
+        return len(set(durations))
+        
+        
