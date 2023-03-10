46a823191ad2eb6b9487a5903a728d2159a0ce5a
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-03-10 19:18:36 +0300

    Time: 38 ms (39.67%), Space: 13.9 MB (20.30%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -6,48 +6,36 @@
 #         self.right = right
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        def choose(node_path, val_path, node):
-            prev = node_path.copy()
+        def choose(val_path, node):
             val_prev = val_path.copy()
-            prev.append(node)
-            
-            
             val_prev.append("->")
             val_prev.append(str(node.val))
-            
-            next_level.append(prev)
             next_path.append(val_prev)
             
             return
         
-        
-        level, all_pathes = [[root]], []
+        level, all_pathes = [root], []
         curr_path = [[str(root.val)]]
-        curr = deepcopy(level)
         
         while level:
-            next_level = []
-            next_path = []
-            for idx, node_path in enumerate(level):
+            next_level, next_path = [], []
+            for idx, node in enumerate(level):
                 val_path = curr_path[idx]
-                last_child = node_path[-1]
         
-                if not last_child.left and not last_child.right:
+                if not node.left and not node.right:
                     all_pathes.append(val_path)
                     
-                if last_child.left:
-                    choose(node_path, val_path, last_child.left)
+                if node.left:
+                    next_level.append(node.left)
+                    choose(val_path, node.left)
                     
-                if last_child.right:
-                    choose(node_path, val_path, last_child.right)
-                    
-                    
-            level = next_level
-            curr_path = next_path
-            
+                if node.right:
+                    next_level.append(node.right)
+                    choose(val_path, node.right)
+                         
+            level, curr_path = next_level, next_path
             
         for idx, path in enumerate(all_pathes):
             all_pathes[idx] = "".join(path)
-            
-                    
-        return all_pathes     
+              
+        return all_pathes
