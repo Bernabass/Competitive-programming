4b9ebd44e13041c20fa138c6fd3947c291a2588c
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-03-20 15:33:13 +0300

    Time: 35 ms (85.07%), Space: 14.4 MB (5.00%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,69 +1,40 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        
         total = sum(nums)
         memo = defaultdict(list)
         
         def pick(start, end):
+            bound = [start, end]
+            if (start, end) in memo:
+                return memo[(start, end)]
             
             if start == end:
                 return 0, 0
             
             if end - start == 1:
-                
                 return nums[start], nums[start]
             
-            bound = [start, end]
+            
             left = nums[start]
             start += 1
-            temp = (start, end)
-            
-            if temp in memo:
-                ptr = memo[temp][0]
+
+            if start == pick(start, end)[0]: start += 1
                 
-            else:
-                picked = pick(start, end)
-                ptr = picked[0]
+            else: end -= 1
             
-            if start == ptr:
-                start += 1
-            
-            else:
-                end -= 1
-            
-            temp = (start, end)
-            if temp in memo:
-                left_sum = left + memo[temp][1]
-                
-            else:
-                picked = pick(start, end)
-                left_sum = left + picked[1]
+            picked = pick(start, end)
+            left_sum = left + picked[1]
              
             start, end = bound
             right = nums[end - 1]
             end -= 1
-            temp = (start, end)
+                        
+            if start == pick(start, end)[0]: start += 1
             
-            if temp in memo:
-                ptr = memo[temp][0]
-                
-            else:
-                picked = pick(start, end)
-                ptr = picked[0]
+            else: end -= 1
             
-            if start == ptr:
-                start += 1
-            
-            else:
-                end -= 1
-            
-            temp = (start, end)
-            if temp in memo:
-                right_sum = right + memo[temp][1]
-                
-            else:
-                picked = pick(start, end)
-                right_sum = right + picked[1]
+            picked = pick(start, end)
+            right_sum = right + picked[1]
                 
             if left_sum > right_sum: 
                 memo[bound[0], bound[1]] = (bound[0], left_sum)
@@ -72,8 +43,6 @@ class Solution:
             else:
                 memo[bound[0], bound[1]] = (bound[1]-1, right_sum)
                 return bound[1]-1, right_sum
-                
-           
+                 
         final = pick(0, len(nums))[1]
-        
         return final >= total - final
