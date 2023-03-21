a605a79fb760e1af19404681f8591ab486ce3130
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-03-21 08:10:41 +0300

    Time: 182 ms (31.87%), Space: 21.2 MB (5.14%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -1,17 +1,26 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        def search(arr, target):
-            idx = bisect_left(arr, target)
-            
-            if idx != len(arr) and arr[idx] == target:
-                return idx
-            
-            return -1
-
-        for row in matrix:
-            if search(row, target) != -1:
-                return True
-            
-        return False
         
+        def search(left, up, right, down):
+            
+            if left > right or up > down:
+                return False
+            
+            elif target < matrix[up][left] or target > matrix[down][right]:
+                return False
+            
+            
+            mid = (left + right) // 2
+            row = up
+            
+            while row <= down and matrix[row][mid] <= target:
+                if matrix[row][mid] == target:
+                    return True
+                
+                row += 1
+                
+            return search(left, row, mid-1, down) or search(mid+1, up, right, row-1)
         
+        return search(0, 0, len(matrix[0])-1, len(matrix)-1)
+            
+            
