af6ff1212e5493216a7b460f4a7286eaaf64fdbd
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-03-21 11:25:26 +0300

    Time: 42 ms (88.35%), Space: 14 MB (12.70%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -1,36 +1,50 @@
 class Solution:
     def totalNQueens(self, n: int) -> int:
-        board = [[0]*n for _ in range(n)]
+        # Use bit manipulation to represent the board
+        board = 0
         
-        def attacked_squares(i, j, place):
-            neighbours = [[1,-1],[0,1],[1,0],[1,1]]
-            if place:
-                board[i][j] += 1
-            else:
-                board[i][j] -= 1
+        # Count of queens attacking each column, diagonal, and anti-diagonal
+        col_attacks = [0] * n
+        diag_attacks = [0] * (2 * n - 1)
+        anti_diag_attacks = [0] * (2 * n - 1)
+        
+        # Backtracking function
+        def backtrack(row=0, count=0):
+            nonlocal board
+            nonlocal col_attacks
+            nonlocal diag_attacks
+            nonlocal anti_diag_attacks
             
-            for r, c in neighbours:
-                row, col = i, j
-                while 0 <= row+r < n and 0 <= col+c < n:
-                    row, col = row+r, col+c 
-                    if place:
-                        board[row][col] += 1
-                        
-                    else:
-                        board[row][col] -= 1
-                        
-        def backtrack(row = 0, count = 0):
-            for col in range(n):
-                if not board[row][col]:
-                    attacked_squares(row, col, 1)
-                                     
-                    if row + 1 == n:
-                        count += 1
-                    else:
-
-                        count = backtrack(row + 1, count)
-                    attacked_squares(row, col, 0)
+            if row == n:
+                # Valid solution found
+                count += 1
+            else:
+                # Try placing a queen in each column of the current row
+                for col in range(n):
+                    col_attack_idx = col
+                    diag_attack_idx = row + col
+                    anti_diag_attack_idx = n - 1 - row + col
+                    
+                    # Check if the column, diagonal, and anti-diagonal are attacked
+                    if col_attacks[col_attack_idx] or diag_attacks[diag_attack_idx] or anti_diag_attacks[anti_diag_attack_idx]:
+                        continue
+                    
+                    # Place the queen and update the attack counts
+                    board ^= (1 << col)
+                    col_attacks[col_attack_idx] = 1
+                    diag_attacks[diag_attack_idx] = 1
+                    anti_diag_attacks[anti_diag_attack_idx] = 1
+                    
+                    # Move to the next row
+                    count = backtrack(row+1, count)
+                    
+                    # Remove the queen and update the attack counts
+                    board ^= (1 << col)
+                    col_attacks[col_attack_idx] = 0
+                    diag_attacks[diag_attack_idx] = 0
+                    anti_diag_attacks[anti_diag_attack_idx] = 0
                     
             return count
         
+        # Start the backtracking search
         return backtrack()
