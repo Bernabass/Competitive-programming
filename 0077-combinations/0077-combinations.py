bc1080de3aa66e2a4d0503b10b08a37bac0f2396
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-03-24 15:39:41 +0300

    Time: 74 ms (98.78%), Space: 15.8 MB (96.18%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,21 +1,3 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        ans = []
-        
-        def back_track(start, depth, pos_ans):
-        
-            if depth == k:
-                ans.append(pos_ans.copy())
-                return
-               
-    
-            for curr in range(start, n+1):
-                pos_ans.append(curr)
-                back_track(curr+1, depth + 1, pos_ans)
-                pos_ans.pop()
-                
-            return
-        
-        back_track(1, 0, [])
-        
-        return ans
+        return list(combinations(range(1, n+1), k))
