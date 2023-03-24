74fb43230519019139dae4a934d6fac0a8aad5fb
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-03-24 16:11:30 +0300

    Time: 414 ms (55.31%), Space: 16 MB (15.33%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,3 +1,26 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        return combinations(range(1, n+1), k)
+        ans = []
+        
+        def comb(prev, ketay):
+            if len(prev) == k:
+                ans.append(prev.copy())
+                return
+            
+            if ketay == n:
+                return
+                
+            
+            ketay += 1
+            prev.append(ketay)
+            comb(prev, ketay)
+                
+            prev.pop()
+            comb(prev, ketay)
+            
+            return
+        
+        
+        comb([], 0)
+        
+        return ans
