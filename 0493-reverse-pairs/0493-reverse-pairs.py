efb9b5274e97a90ef67864206224b337d42fe4f6
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-03-25 11:18:48 +0300

    Time: 1969 ms (49.41%), Space: 21.6 MB (51.75%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.py
+++ 0493-reverse-pairs/0493-reverse-pairs.py
@@ -1,29 +1,21 @@
 class Solution:
     def reversePairs(self, nums: List[int]) -> int:
         count = [0]
-        def divide(arr):
+        def merge_sort(arr):
             if len(arr) ==  1:
                 return arr
             
             mid = len(arr) // 2
-            left = divide(arr[:mid])
-            right = divide(arr[mid:])
-            
-            start = 0
-            end = len(right) - 1
-            for num in left:
-                res = search(right, start, end, num)
-                count[0] += res
-                start = res
-                if res > 0:
-                    start = res - 1
+            left = merge_sort(arr[:mid])
+            right = merge_sort(arr[mid:])
+            count[0] += find_pairs(left, right)
             
             return merge(left, right)
             
             
         def merge(arr1, arr2):
             n1, n2 = len(arr1), len(arr2)
-            ptr1 = ptr2 = 0
+            ptr1 = ptr2 = next_valid = 0
             result = []
 
             while ptr1 < n1 and ptr2 < n2:
@@ -31,7 +23,7 @@ class Solution:
                     result.append(arr1[ptr1])
                     ptr1 += 1
 
-                else:
+                else:  
                     result.append(arr2[ptr2])
                     ptr2 += 1
 
@@ -40,25 +32,21 @@ class Solution:
 
             return result
         
-        def search(arr, left, right, target):
-            org = left
-            while left <= right:
-
-                mid = (left+right) // 2
-                if mid >= len(arr):
-                    break
-                if arr[mid]*2 < target:
-                    if mid+1 == len(arr):
-                        return mid+1
-                    if arr[mid+1]*2 >= target:
-                        return mid+1
-
-                    left = mid+1
-
+        def find_pairs(arr1, arr2):
+            n1, n2 = len(arr1), len(arr2)
+            ptr1 = ptr2 = valids = 0
+            
+            while ptr1 < n1 and ptr2 < n2:
+                
+                if arr2[ptr2]*2 < arr1[ptr1]:
+                    valids += len(arr1) - ptr1
+                    ptr2 += 1
+                    
                 else:
-                    right = mid - 1
-
-            return org
+                    ptr1 += 1
+            
+            return valids
         
-        divide(nums)
+        merge_sort(nums)
+        
         return count[0]
