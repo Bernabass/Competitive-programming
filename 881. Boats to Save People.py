6c9f1e761cbdd54fccadec8870cddbd61a480fb6
Naol Taye <naoltd164@gmail.com>
2023-04-03 22:37:15 +0300

    medium

---
+++ 881. Boats to Save People.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort(reverse = True)
+        left = boats = 0
+        right = len(people) - 1
+        
+        while left <= right:
+            if people[left] + people[right] <= limit:
+                left += 1
+                right -= 1   
+            else:
+                left += 1
+                
+            boats += 1   
+            
+        return boats
