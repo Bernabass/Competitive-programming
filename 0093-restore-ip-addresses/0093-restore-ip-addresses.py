74f0c121ade9eaf3133dc1890048cb2efc479ccd
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-04-04 10:32:42 +0300

    Time: 37 ms (64.31%), Space: 13.9 MB (24.62%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,37 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        n = len(s)
+        ans = set()
+        
+        def back_track(start, prev):
+            if start == n:
+                if len(prev) == 4:
+                    ans.add(".".join(prev))
+                return
+            
+            
+            for i in range(start, n):
+                if len(prev) < 3:
+                    curr = int(s[start:i+1])
+                    if str(curr) != s[start:i+1]:
+                        break
+                    
+                else:
+                    curr = int(s[start:])
+                    if str(curr) != s[start:]:
+                        break
+                    i = n - 1
+                
+                if 0 <= curr <= 255:
+                    prev.append(str(curr))
+                    back_track(i+1, prev)
+                    prev.pop()
+                    
+                else:
+                    break
+                    
+            return
+        
+        back_track(0, [])
+        
+        return ans
