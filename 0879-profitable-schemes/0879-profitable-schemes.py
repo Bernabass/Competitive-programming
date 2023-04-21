ddb9cc9e3cd7151540bcc660b43c02b29dbcec91
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-04-21 22:44:26 +0300

    Time: 4246 ms (18.27%), Space: 661.1 MB (8.65%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.py
+++ 0879-profitable-schemes/0879-profitable-schemes.py
@@ -1,28 +1,16 @@
 class Solution:
     def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
-        memo = []
-        for i in range(len(group) + 1):
-            memo_i = []
-            for j in range(minProfit + 1):
-                memo_i.append([-1] * (n + 1))
-            memo.append(memo_i)
-
-        def count(crime_i: int, curr_prof: int, curr_mem: int) -> int:
+        @cache
+        def count(crime_i, curr_prof, curr_mem):
             if curr_mem > n:
                 return 0
             if crime_i >= len(group):
                 return int(curr_prof >= minProfit and curr_mem <= n)
             
-            if memo[crime_i][curr_prof][curr_mem] != -1:
-                return memo[crime_i][curr_prof][curr_mem]
             
             not_pick = count(crime_i + 1, curr_prof, curr_mem)
-            pick = count(crime_i + 1, min(minProfit, curr_prof + profit[crime_i]), curr_mem + group[crime_i])
-            
-            memo[crime_i][curr_prof][curr_mem] = (not_pick + pick) % (10 ** 9 + 7)
-            
-            return memo[crime_i][curr_prof][curr_mem]
+            pick = count(crime_i+1, min(minProfit, curr_prof+profit[crime_i]), curr_mem+ group[crime_i])
+                
+            return  (not_pick + pick) % (10 ** 9 + 7)
 
         return count(0, 0, 0)
-
-
