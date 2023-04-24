2d8abec3a11db19ed06ed3ad38ce6a9ce129d58c
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-04-24 10:02:13 +0300

    Time: 2052 ms (25.97%), Space: 165.8 MB (29.87%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.py
+++ 1416-restore-the-array/1416-restore-the-array.py
@@ -1,23 +1,22 @@
 class Solution:
     def numberOfArrays(self, s: str, k: int) -> int:
-        n= len(s)
-
+        
         @cache
-        def dfs(idx):
+        def back_track(idx):
             if not idx:
                 return 1
 
             ans = 0
             for i in range(idx-1, -1, -1):
-                if s[i] == "0": 
+                if not(int(s[i])): 
                     continue
                     
                 elif int(s[i:idx]) <= k:
-                    ans += dfs(i) % (10**9 + 7)
+                    ans += back_track(i) % (10**9 + 7)
                     
                 elif int(s[i:idx]) > k:
                     break
-
+            
             return ans
-
-        return dfs(n) % (10**9+7)
+        
+        return back_track(len(s)) % (10**9 + 7)
