86ee7ced5b31278757fedb781487ce817a96f1d3
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-05-01 17:49:31 +0300

    Time: 75 ms (40.66%), Space: 18 MB (5.11%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -2,10 +2,9 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         x = y = 0
         
-        
         for num in nums:
-            a = (x & ~y & ~num) | (~x & y & num)
-            b = (~x & ~y & num) | (~x & y & ~num)
+            a = (x & ~num) | (~x & y & num)
+            b = ~x & (y ^ num)
             x, y = a, b
             
         return x | y
