e5cdc9e5afb148a21d67b607e96f50924283e394
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-05-05 21:20:48 +0300

    Time: 49 ms (20.40%), Space: 16.5 MB (7.37%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,27 +1,22 @@
 class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
+    def permute(self, nums: List[int]) -> List[List[int]]:    
+        ans = []
         
-        def insert(num, arr):
-            total = []
-
-            for curr in arr:
-                n = len(curr)+1
-                for count in range(n):
-                    ans, idx = [11]*n, 0
-                    ans[count] = num
-                    
-                    for i, val in enumerate(ans):
-                        if val == 11:
-                            ans[i] = curr[idx]
-                            idx += 1
+        def back_track(used, res):
+            if len(used) == len(nums):
                 
-                    total.append(ans)
-                    
-            return total
-        
-        level = [[nums[0]]]
-        
-        for i in range(1, len(nums)):
-            level = insert(nums[i], level)
+                ans.append(res.copy())
             
-        return level
+            for num in nums:
+                if num not in used:
+                    used.add(num)
+                    res.append(num)
+                    back_track(used, res)
+                    used.remove(num)
+                    res.pop()
+                    
+            return
+        
+        back_track(set(), [])
+        
+        return ans
