14aa018c58766a977a4a6b88f2c251a65c5c8c45
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-05-05 16:55:14 +0300

    Time: 533 ms (73.63%), Space: 16.2 MB (31.59%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
@@ -1,28 +1,17 @@
 class Solution:
     def countMaxOrSubsets(self, nums: List[int]) -> int:
-        max_OR, count = nums[0], [0]
-        ans, n = [], len(nums)
+        max_OR = reduce(or_, nums)
         
-        for i in range(1, n):
-            max_OR |= nums[i]
-        
-        def back_track(idx, prev):
-            if idx == n:
-                return
+        def back_track(idx, prev, count):
+            if idx == len(nums):
+                return 0
             
-            for i in range(idx, n):
-                temp = prev
-                prev |= nums[i]
-                
-                if prev == max_OR:
-                    count[0] += 1
+            for i in range(idx, len(nums)):
+                if prev|nums[i] == max_OR:
+                    count += 1
                     
-                back_track(i+1, prev)
-                
-                prev = temp
+                count += back_track(i+1, prev|nums[i], 0)
                    
-            return
+            return count
         
-        back_track(0, 0)
-        
-        return count[0]
+        return back_track(0, 0, 0)
