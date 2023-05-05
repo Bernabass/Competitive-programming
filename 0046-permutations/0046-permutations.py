8373305decc66bd8fee3f262caf63ef629df4a58
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-05-05 21:33:16 +0300

    Time: 56 ms (7.48%), Space: 16.4 MB (7.37%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,20 +1,14 @@
 class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:    
-        ans = []
-        
-        def back_track(arr, res):
-            if not nums:
-                ans.append(res.copy())
+    def permute(self, nums: List[int]) -> List[List[int]]:            
+        res = []
+        def back_track(path, num):
+            if not num:
+                res.append(path)   
+                return
             
-            for idx, num in enumerate(nums):
-                res.append(num)
-                nums.remove(num)
-                back_track(nums, res)
-                nums.insert(idx, num)
-                res.pop()
-                
-            return
+            for i in range(len(num)):
+                back_track(path + [num[i]], num[:i] + num[i+1:]) 
         
-        back_track(nums, [])
+        back_track([], nums)
         
-        return ans
+        return res
