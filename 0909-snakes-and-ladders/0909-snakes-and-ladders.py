8e83bbdba511ce983dfc8904fa18f2dd0688b93c
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-05-05 19:11:44 +0300

    Time: 120 ms (66.96%), Space: 16.4 MB (5.86%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.py
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -1,52 +1,40 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-        n, seen = len(board), set()
+        n = len(board)
         
         def coordinate(val):        
-            row = (val - 1) // n
-            row = n - row - 1
-
+            row = n - ((val - 1) // n) - 1
             col = (val - 1) % n
-            if (n- row) % 2 == 0:
+            
+            if not((n- row) % 2):
                 col = n - col - 1
 
             return row, col
-        
-        def roll_die(curr):
-            start, end = [curr+1, min(curr+6, n**2)]
+           
+        level, seen, depth = [1], {0}, 0
+            
+        while level:
+            depth += 1
+            next_level = []
 
-            return [value for value in range(start, end+1)]
-            
-        def bfs(node):
-            level, depth = [node], 0
-            seen.add(node)
-            
-            while level:
-                depth += 1
-                next_level = []
-                
-                for value in level:
-                    destination = roll_die(value)
-                    
-                    for adj in destination:
-                        if adj not in seen:
-                            if adj == n**2:
+            for curr in level:
+                for adj in range(curr+1, min(curr+6, n**2)+1):
+                    if adj not in seen:
+                        if adj == n**2:
+                            return depth
+
+                        i, j = coordinate(adj)
+                        rep = board[i][j]
+                        if rep == -1:
+                            next_level.append(adj)
+
+                        else:
+                            if rep == n**2:
                                 return depth
+                            next_level.append(rep)
                             
-                            seen.add(adj)
-                            
-                            i, j = coordinate(adj)
-                            rep = board[i][j]
-                            if rep == -1:
-                                next_level.append(adj)
-                                
-                            else:
-                                if rep == n**2:
-                                    return depth
-                                next_level.append(rep)
-                                    
-                level = next_level
-                
-            return -1
-                
-        return bfs(1)
+                        seen.add(adj)
+
+            level = next_level
+
+        return -1
