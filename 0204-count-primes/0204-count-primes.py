4ad217c1bfa973edf36635aed9b553cdacefb402
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-05-06 21:55:35 +0300

    Time: 4673 ms (37.49%), Space: 55.1 MB (53.81%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -5,10 +5,12 @@ class Solution:
  
         is_prime = [1] * n
         is_prime[0] = is_prime[1] = 0
+        count = 2
  
         for i in range(2, int(n**0.5)+1):
             if is_prime[i]:
                 for j in range(i**2, n, i):
+                    count += is_prime[j]
                     is_prime[j] = 0
-        
-        return sum(is_prime)
+                    
+        return n - count
