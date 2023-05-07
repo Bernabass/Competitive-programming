d7a456372d7fae7f39d01af86e40fdfc05ab64f4
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-05-07 22:43:59 +0300

    Time: 188 ms (29.66%), Space: 17.7 MB (14.55%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -2,24 +2,25 @@ class Solution:
     def minSteps(self, n: int) -> int:
         if n == 1:
             return 0
-        factors = self.factors(n)
-        ans = len(factors) - 1
+
         
-        for i in range(len(factors) - 1):
-            ans += (factors[i] // factors[i+1]) - 1
-            
-        return ans + factors[-1]
-            
-    def factors(self, n):
-        factors = []
-        d = 2
+        level, seen = [(0, 1)], {(0, 1)}
+        depth = 0
 
-        while d * d <= n:
-            while n % d == 0:
-                factors.append(n)
-                n //= d
-            d += 1
-        if n > 1:
-            factors.append(n)
-
-        return factors
+        while level:
+            depth += 1
+            next_level = []
+            
+            for copy, paste in level:
+                if (copy+paste, copy) not in seen and paste+copy <= 1000:
+                    if copy+paste == n:
+                        return depth
+                    
+                    next_level.append((copy, copy+paste))
+                    seen.add((copy, copy+paste))
+                    
+                if (paste, paste) not in seen and paste <= 1000:
+                    next_level.append((paste, paste))
+                    seen.add((paste, paste))
+                    
+            level = next_level
