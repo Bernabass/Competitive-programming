0d4c16ab03b251ab26c736fb96a01fd9a3e67ca5
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-05-08 13:36:24 +0300

    Time: 80 ms (6.91%), Space: 16.3 MB (15.99%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,8 +1,18 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        count = Counter(words)
-        freq = [-val for val in count.values()]
-        pair = list(tuple(zip(freq, count.keys())))
-        heapify(pair)        
-        
-        return [word for val, word in nsmallest(k, pair)]
+        a = {}
+
+        for word in words:
+            if word in a:
+                a[word] += 1
+            else:
+                a[word] = 1
+
+        l = list(a.items())
+        l = [(-j,i) for i,j in l]
+        heapq.heapify(l)
+        ret = []
+        for i in range(k):
+            _,w = heapq.heappop(l)
+            ret.append(w)
+        return ret
