a290399899b9f1f99e56116f245308eeab8dd9d7
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-05-08 18:33:54 +0300

    Time: 113 ms (22.19%), Space: 17.8 MB (17.89%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        GRAPH, INDEGREE = defaultdict(list), defaultdict(int)
+        order, level = [], []
+        
+        for child, parent in prerequisites:
+            GRAPH[parent].append(child)
+            INDEGREE[child] += 1
+                           
+        for node in range(numCourses):
+            if not(INDEGREE[node]):
+                level.append(node)
+         
+        while level:
+            next_level = []
+
+            for node in level:
+                order.append(node)
+                for adj in GRAPH[node]:
+                    if INDEGREE[adj]:
+                        INDEGREE[adj] -= 1
+                        
+                        if not (INDEGREE[adj]):
+                            next_level.append(adj)
+                            
+            level = next_level
+            
+        return order if len(order) == numCourses else []
