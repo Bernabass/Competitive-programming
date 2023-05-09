1f97a4c10303661b297f72e5f23b660f6fdcba39
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-05-09 03:11:20 +0300

    Time: 1956 ms (7.37%), Space: 51.2 MB (24.78%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -1,25 +1,24 @@
 class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
         GRAPH = defaultdict(list)
-        answer = [set() for _ in range(n)]
-        
+        answer = [0] * n
+
         for src, dest in edges:
             GRAPH[dest].append(src)
-            
-            
-        def bfs(node, seen):
+
+        def bfs(node):
             level = [node]
-            seen.add(node)
+            seen = 1 << node
             org = node
-            
+
             while level:
                 next_level = []
-                
+
                 for node in level:
                     for adj in GRAPH[node]:
-                        if adj not in seen:
-                            answer[org].add(adj)
-                            seen.add(adj)
+                        if not seen & (1 << adj):
+                            answer[org] |= 1 << adj
+                            seen |= 1 << adj
 
                             if answer[adj]:
                                 answer[org] |= answer[adj]
@@ -28,8 +27,8 @@ class Solution:
                                 next_level.append(adj)
 
                 level = next_level
-        
+
         for node in range(n):
-            bfs(node, set())
-            
-        return [sorted(list(curr)) for curr in answer]
+            bfs(node)
+
+        return [[i for i in range(n) if (1 << i) & curr] for curr in answer]
