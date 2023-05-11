32b1d937d5ec81e84488d8412d78585191d4cf1a
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-05-11 11:32:57 +0300

    Time: 448 ms (71.93%), Space: 26 MB (18.42%) - LeetHub

--- 0851-loud-and-rich/0851-loud-and-rich.py
+++ 0851-loud-and-rich/0851-loud-and-rich.py
@@ -1,24 +1,25 @@
 class Solution:
     def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
-        GRAPH, seen = defaultdict(list), set()
-        answer = list(range(len(quiet)))
+        GRAPH, n = defaultdict(list), len(quiet)
+        answer = [501] * n
         
         for adj, node in richer:
             GRAPH[node].append(adj)
             
         def dfs(node):
-            if node in seen:
+            if answer[node] != 501:
                 return answer[node]
             
-            seen.add(node)
+            answer[node] = min(node, answer[node])
             for adj in GRAPH[node]:
                 person = dfs(adj)
+                
                 if quiet[person] < quiet[answer[node]]:
                     answer[node] = person
-                    
+                         
             return answer[node]
         
-        for node in range(len(quiet)):
+        for node in range(n):
             answer[node] = dfs(node)
             
         return answer
