b303a8ca95e87f7981432d95ecf448792d2a73f5
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-05-12 19:29:30 +0300

    Time: 1184 ms (59.78%), Space: 153.6 MB (30.64%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
@@ -1,29 +1,28 @@
 class Solution:
     def longestCycle(self, edges: List[int]) -> int:
         self.ans, n = -1, len(edges)
-        arr, colors = [0]*n, [0]*n
+        arr = [0]*n
 
-        
         def dfs(node, val):
-            if colors[node] == 2:
+            if arr[node] == -1:
                 return
             
             arr[node] = val
-            colors[node] = 1
             child = edges[node]
             
-            if child != -1 and colors[child] == 1:
+            if child != -1 and arr[child] not in {0, -1}:
                 self.ans = max(self.ans, val - arr[child] + 1)
-                colors[node] = 2
+                arr[node] = -1
+                
                 return 
                 
             if child != -1:
                 dfs(child, val + 1)
             
-            colors[node] = 2
+            arr[node] = -1
                  
         for num in range(len(edges)):
-            if not colors[num]:
+            if not arr[num]:
                 dfs(num, 1)
            
         return self.ans
