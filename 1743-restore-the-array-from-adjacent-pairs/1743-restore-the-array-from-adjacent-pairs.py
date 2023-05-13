0791f0ca1892558f532498088d0e3257cbcc4d81
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-05-13 13:56:11 +0300

    Time: 1106 ms (78.57%), Space: 67.1 MB (44.80%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
@@ -1,24 +1,26 @@
 class Solution:
     def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
         GRAPH, seen = defaultdict(list), set()
-        original = []
+        original, queue = [], deque()
         
         for node, adj in adjacentPairs:
             GRAPH[node].append(adj)
             GRAPH[adj].append(node)
-            
-        def dfs(node, parent):
-            seen.add(node)
-            original.append(node)
-
-            for adj in GRAPH[node]:
-                if adj != parent and adj not in seen:
-                    dfs(adj, node)
-            
+                        
             
         for node, adj in GRAPH.items():
             if len(adj) == 1:
-                dfs(node, "front")
+                queue.append(node)
                 break
+                
+                
+        while queue:
+            node = queue.popleft()
+            original.append(node)
+            seen.add(node)
+            
+            for adj in GRAPH[node]:
+                if adj not in seen:
+                    queue.append(adj)
                 
         return original
