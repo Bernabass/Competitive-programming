43da945dab9aabad2b4d244545c4452c04ee1c30
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-05-26 19:32:01 +0300

    Time: 884 ms (71.18%), Space: 225.1 MB (8.22%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -7,21 +7,13 @@ class Solution:
             return False
         
         target, n = total // 2, len(nums)
-        ans = [False]
         
         @cache
         def back_track(idx, curr_sum):
-            if curr_sum == target:
-                ans[0] = True
-                
-            if idx == n or curr_sum > target:
-                return
+          
+            if idx == n or curr_sum >= target:
+                return curr_sum == target
             
-            back_track(idx+1, curr_sum)
-            back_track(idx+1, curr_sum + nums[idx])
-            
-            return
+            return back_track(idx+1, curr_sum) or back_track(idx+1, curr_sum + nums[idx])
         
-        back_track(0, 0)
-        
-        return ans[0]
+        return back_track(0, 0)
