59022c58f994bec7e27a536a50ff4a8791bc7381
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-05-26 17:41:57 +0300

    Time: 42 ms (55.59%), Space: 16.4 MB (5.49%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        @cache
+        def rec(n):
+            if n == 0:
+                return 0
+            
+            if n == 1:
+                return 1
+
+            return rec(n-1) + rec(n-2)
+        
+        return rec(n)
