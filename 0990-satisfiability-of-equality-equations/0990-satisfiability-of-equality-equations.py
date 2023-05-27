fd84a2b5cdf2e6c1d4649cacec8212fb5eb306c1
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-05-28 01:04:54 +0300

    Time: 69 ms (13.19%), Space: 16.5 MB (28.71%) - LeetHub

---
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
@@ -0,0 +1,76 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = {}
+        self.rank, self.size = {}, {}
+        self.count = n
+        
+    def add(self, x):
+        if x not in self.parent:
+            self.parent[x] = x
+            self.rank[x] = 0
+            self.size[x] = 1
+
+    def find(self, x):
+        original = x
+        while self.parent[x] != x:
+            x = self.parent[x]
+
+        while self.parent[original] != original:
+            original = self.parent[original]
+            self.parent[original] = x
+
+        return x
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+
+        if root_x == root_y:
+            return
+
+        if self.rank[root_x] < self.rank[root_y]:
+            self.parent[root_x] = root_y
+            self.size[root_y] += self.size[root_x]
+        elif self.rank[root_x] > self.rank[root_y]:
+            self.parent[root_y] = root_x
+            self.size[root_x] += self.size[root_y]
+        else:
+            self.parent[root_y] = root_x
+            self.size[root_x] += self.size[root_y]
+            self.rank[root_x] += 1
+        
+        self.count -= 1
+     
+    def is_connected(self, x, y):
+        return self.find(x) == self.find(y)   
+    
+    def count(self):
+        return self.count
+
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        equals = UnionFind(26)
+        temp = []
+        for i in range(97, 123):
+            equals.add(chr(i))
+            
+        for x in equations:
+            if x[1] == "=":
+                temp.append(x)
+                
+        for x in equations:
+            if x[1] != "=":
+                temp.append(x)
+ 
+        
+        for string in temp:
+            x, sign, y = string[0], string[1], string[3]
+            
+            if sign == "=":
+                equals.union(x, y)
+                
+            else:
+                if equals.is_connected(x, y):
+                    return False
+                
+        return True
