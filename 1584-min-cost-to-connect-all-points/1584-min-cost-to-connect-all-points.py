e802b417ae8ba17d9c85a243e7b3e876f4c86622
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-05-29 17:03:35 +0300

    Time: 494 ms (99.92%), Space: 17 MB (93.48%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,3 +1,45 @@
+class parent(defaultdict):
+    def __missing__(self, key):
+        return key
+class UnionFind:
+    def __init__(self):
+        self.parent = parent()
+        self.rank, self.size = defaultdict(int), defaultdict(lambda:1)
+        
+    def find(self, x):
+        original = x
+        while self.parent[x] != x:
+            x = self.parent[x]
+
+        while self.parent[original] != original:
+            original = self.parent[original]
+            self.parent[original] = x
+
+        return x
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+
+        if root_x == root_y:
+            return
+
+        if self.rank[root_x] < self.rank[root_y]:
+            self.parent[root_x] = root_y
+            self.size[root_y] += self.size[root_x]
+        elif self.rank[root_x] > self.rank[root_y]:
+            self.parent[root_y] = root_x
+            self.size[root_x] += self.size[root_y]
+        else:
+            self.parent[root_y] = root_x
+            self.size[root_x] += self.size[root_y]
+            self.rank[root_x] += 1
+        
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)   
+    
+    def count(self):
+        return len({self.find(x) for x in list(self.parent)})
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         output = 0
