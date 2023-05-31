fb0db768d2daf8bb665ad029ab62e2c56bd06682
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-05-31 11:48:46 +0300

    Time: 1628 ms (47.93%), Space: 16.3 MB (60.44%) - LeetHub

--- 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.py
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.py
@@ -2,9 +2,8 @@ class Solution:
     def punishmentNumber(self, n: int) -> int:
         ans = 0
         def back_track(idx, prev, target, s):
-            if idx == len(s):
-                if prev == target:
-                    return True
+            if idx == len(s) and prev == target:
+                return True
                 
             for i in range(idx, len(s)):
                 curr = int(s[idx:i+1])
@@ -12,9 +11,6 @@ class Solution:
                 if curr + prev <= target:
                     if back_track(i+1, curr+prev, target, s):
                         return True
-                    
-                else:
-                    return False
                     
         for i in range(1, n+1):
             pos_ans = i**2
