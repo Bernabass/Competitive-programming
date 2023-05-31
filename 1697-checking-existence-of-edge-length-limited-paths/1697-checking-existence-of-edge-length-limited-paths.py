1d24a1317043351e0436a1b9489b74597ccb23a9
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-05-31 10:52:07 +0300

    Time: 2195 ms (5.03%), Space: 63.9 MB (9.82%) - LeetHub

--- 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.py
+++ 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.py
@@ -1,34 +1,65 @@
+class parent(defaultdict):
+    def __missing__(self, key):
+        return key
 class UnionFind:
-    def __init__(self, size):
-        self.parent = [i for i in range(size)]
+    def __init__(self):
+        self.parent = parent()
+        self.rank, self.size = defaultdict(int), defaultdict(lambda:1)
         
-    def union(self, a, b):
-        self.parent[self.find(a)] = self.find(b)
+    def find(self, x):
+        original = x
+        while self.parent[x] != x:
+            x = self.parent[x]
+
+        while self.parent[original] != original:
+            original = self.parent[original]
+            self.parent[original] = x
+
+        return x
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+
+        if root_x == root_y:
+            return
+
+        if self.rank[root_x] < self.rank[root_y]:
+            self.parent[root_x] = root_y
+            self.size[root_y] += self.size[root_x]
+        elif self.rank[root_x] > self.rank[root_y]:
+            self.parent[root_y] = root_x
+            self.size[root_x] += self.size[root_y]
+        else:
+            self.parent[root_y] = root_x
+            self.size[root_x] += self.size[root_y]
+            self.rank[root_x] += 1
         
-    def find(self, a):
-        if self.parent[a] != a:
-            self.parent[a] = self.find(self.parent[a])
-            
-        return self.parent[a]
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)   
+    
+    def count(self):
+        return len({self.find(x) for x in list(self.parent)})
 
 class Solution:
     def distanceLimitedPathsExist(self, n: int, edgeList: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        E = len(edgeList)
-        Q = len(queries)
-        uf = UnionFind(n)
-        ans = [False] * Q
-        for i in range(Q):
-            queries[i].append(i)
-            
-        queries.sort(key=lambda x: x[2])
-        edgeList.sort(key=lambda x: x[2])
+        n_queries, n_edges = len(queries), len(edgeList)
+        uf = UnionFind()
+        answer = [False]*n_queries
         
-        j = 0
-        for i in range(Q):
-            while j < E and edgeList[j][2] < queries[i][2]:
-                uf.union(edgeList[j][0], edgeList[j][1])
-                
-                j += 1
-            ans[queries[i][3]] = (uf.find(queries[i][0]) == uf.find(queries[i][1]))
+        for i in range(n_queries):
+            queries[i].append(i)
+         
+        edgeList.sort(key = lambda x:x[2])
+        queries.sort(key = lambda x:x[2])
+        
+        idx = 0
+        for p, q, limit, org_idx in queries:
+            while idx < n_edges and edgeList[idx][2] < limit:
+                u, v, dist = edgeList[idx]
+                uf.union(u, v)
+                idx += 1
             
-        return ans
+            answer[org_idx] = uf.connected(p, q)
+            
+        return answer
