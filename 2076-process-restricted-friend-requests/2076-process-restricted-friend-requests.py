f0d3ba3757bb0e5e76640b2b7ba8eb2bc829a1db
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-06-01 16:55:50 +0300

    Time: 4868 ms (42.86%), Space: 17.3 MB (23.53%) - LeetHub

--- 2076-process-restricted-friend-requests/2076-process-restricted-friend-requests.py
+++ 2076-process-restricted-friend-requests/2076-process-restricted-friend-requests.py
@@ -37,12 +37,6 @@ class UnionFind:
             self.parent[root_y] = root_x
             self.rank[root_x] += 1
         
-    def connected(self, x, y):
-        return self.find(x) == self.find(y)   
-    
-    def count(self):
-        return len({self.find(x) for x in list(self.parent)})
-    
     def setter(self, x, rank):
         self.parent[x] = x
         self.rank[x] = rank
@@ -52,7 +46,6 @@ class Solution:
         
         uf = UnionFind()
         ans = [True] * len(requests)
-        
         for idx, (a, b) in enumerate(requests):
             prev_a, prev_b = uf.find(a), uf.find(b)
             r_a, r_b = uf.rank[a], uf.rank[b]
