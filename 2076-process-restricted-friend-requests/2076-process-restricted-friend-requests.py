e162a1ab8384915f17fe937dc7b14774f0ee5cf7
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-06-01 17:08:39 +0300

    Time: 249 ms (98.32%), Space: 17.2 MB (40.34%) - LeetHub

--- 2076-process-restricted-friend-requests/2076-process-restricted-friend-requests.py
+++ 2076-process-restricted-friend-requests/2076-process-restricted-friend-requests.py
@@ -1,61 +1,38 @@
-class my_dict(defaultdict):
-    def __missing__(self, key):
-        return key
-class UnionFind:
-    def __init__(self):
-        self.parent = my_dict()
-        self.rank = defaultdict(int)
-        
-    def find(self, x):
-        original = x
-        while self.parent[x] != x:
-            x = self.parent[x]
+class UnionFind2076:
+    def __init__(self, size, restrictions):
+        self.parent = [i for i in range(size)]
+        self.restrictions = [0] * size
+        self.all = [(1<<i) for i in range(size)]
+        for a, b in restrictions:
+            self.restrictions[a] |= (1<<b)
+            self.restrictions[b] |= (1<<a)
 
-        while self.parent[original] != original:
-            original = self.parent[original]
-            self.parent[original] = x
+    def find(self, n):
+        m = n
+        while self.parent[m] != m:
+            m = self.parent[m]
+        while self.parent[n] != m:
+            old_parent = self.parent[n]
+            self.parent[n] = m
+            n = old_parent
+        return m
 
-        return x
-    
-    def find2(self, x):
-        while self.parent[x] != x:
-            x = self.parent[x]
-        return x 
-
-    def union(self, x, y):
-        root_x = self.find(x)
-        root_y = self.find(y)
-
-        if root_x == root_y:
-            return
-
-        if self.rank[root_x] < self.rank[root_y]:
-            self.parent[root_x] = root_y
-        elif self.rank[root_x] > self.rank[root_y]:
-            self.parent[root_y] = root_x
-        else:
-            self.parent[root_y] = root_x
-            self.rank[root_x] += 1
-        
-    def setter(self, x, rank):
-        self.parent[x] = x
-        self.rank[x] = rank
+    def union(self, m, n):
+        root_m, root_n = self.find(m), self.find(n)
+        if root_m == root_n:
+            return True
+        if self.restrictions[root_m] & self.all[root_n]:
+            return False
+        self.parent[root_m] = root_n
+        self.all[root_n] |= self.all[root_m]
+        self.restrictions[root_n] |= self.restrictions[root_m]
+        return True
 
 class Solution:
-    def friendRequests(self, n: int, restrictions: List[List[int]], requests: List[List[int]]) -> List[bool]:
-        
-        uf = UnionFind()
-        ans = [True] * len(requests)
-        for idx, (a, b) in enumerate(requests):
-            prev_a, prev_b = uf.find(a), uf.find(b)
-            r_a, r_b = uf.rank[a], uf.rank[b]
-            uf.union(a, b)
-            
-            for i, j in restrictions:
-                if uf.find2(i) == uf.find2(j):
-                    uf.setter(prev_a, r_a)
-                    uf.setter(prev_b, r_b)
-                    ans[idx] = False
-                    break
-            
-        return ans
+    #2076
+    def friendRequests(self, n: int, restrictions, requests):
+        uf = UnionFind2076(n, restrictions)
+        res = []
+        for a,b in requests:
+            res.append(uf.union(a,b))
+        return res
