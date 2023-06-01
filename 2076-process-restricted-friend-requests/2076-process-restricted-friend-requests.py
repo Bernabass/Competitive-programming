b67418ff808c36962feff52ab38ca53ce938fac8
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-06-01 16:48:33 +0300

    Time: 5566 ms (36.98%), Space: 17.3 MB (23.53%) - LeetHub

--- 2076-process-restricted-friend-requests/2076-process-restricted-friend-requests.py
+++ 2076-process-restricted-friend-requests/2076-process-restricted-friend-requests.py
@@ -1,31 +1,64 @@
+class my_dict(defaultdict):
+    def __missing__(self, key):
+        return key
+class UnionFind:
+    def __init__(self):
+        self.parent = my_dict()
+        self.rank, self.size = defaultdict(int), defaultdict(lambda:1)
+        
+    def find(self, x):
+        original = x
+        while self.parent[x] != x:
+            x = self.parent[x]
+
+        # while self.parent[original] != original:
+        #     original = self.parent[original]
+        #     self.parent[original] = x
+
+        return x
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+
+        if root_x == root_y:
+            return
+
+        if self.rank[root_x] < self.rank[root_y]:
+            self.parent[root_x] = root_y
+            self.size[root_y] += self.size[root_x]
+        elif self.rank[root_x] > self.rank[root_y]:
+            self.parent[root_y] = root_x
+            self.size[root_x] += self.size[root_y]
+        else:
+            self.parent[root_y] = root_x
+            self.size[root_x] += self.size[root_y]
+            self.rank[root_x] += 1
+        
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)   
+    
+    def count(self):
+        return len({self.find(x) for x in list(self.parent)})
+    
+    def setter(self, x, par):
+        self.parent[x] = par
+
 class Solution:
     def friendRequests(self, n: int, restrictions: List[List[int]], requests: List[List[int]]) -> List[bool]:
-        parent = list(range(n))
         
-        def find(node):
-            while node != parent[node]:
-                node = parent[node]
-                
-            return node
+        uf = UnionFind()
+        ans = [True] * len(requests)
         
-        def union(a, b, m):
-            pa, pb = find(a), find(b)
-            if pa == pb:
-                return
-            parent[pb] = pa
+        for idx, (a, b) in enumerate(requests):
+            prev_a, prev_b = uf.find(a), uf.find(b)
+            uf.union(a, b)
             
             for i, j in restrictions:
-                if find(i) == find(j):
-                    ans[m] = False
-                    parent[pb] = pb
+                if uf.connected(i, j):
+                    uf.setter(prev_a, prev_a)
+                    uf.setter(prev_b, prev_b)
+                    ans[idx] = False
                     break
-            
-        
-            
-        ans = [True]*len(requests)
-
-        for i in range(len(requests)):
-            a, b = requests[i][0], requests[i][1]
-            union(a, b, i)
             
         return ans
