81208afbefac4a410732bca8272b5be8e0b2248d
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-06-02 14:19:27 +0300

    Time: 530 ms (26.12%), Space: 32.9 MB (5.06%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
@@ -1,8 +1,11 @@
 class Solution:
     def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
         freq = Counter(arr)
-        can_change = sum(num > len(arr) for num in arr)
+        can_change = 0
         n = ans = len(arr)
+        for num in arr:
+            if num > n:
+                can_change += 1
         
         for i in range(n, 0, -1):
             if not freq[i]:
