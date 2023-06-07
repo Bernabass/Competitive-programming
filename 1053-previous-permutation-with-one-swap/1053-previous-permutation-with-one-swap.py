b6e2d78a24236622921a195f6328b89999dff1e0
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-06-07 17:13:36 +0300

    Time: 235 ms (59.76%), Space: 17.6 MB (69.92%) - LeetHub

--- 1053-previous-permutation-with-one-swap/1053-previous-permutation-with-one-swap.py
+++ 1053-previous-permutation-with-one-swap/1053-previous-permutation-with-one-swap.py
@@ -1,20 +1,12 @@
 class Solution:
     def prevPermOpt1(self, arr: List[int]) -> List[int]:
-        ind = len(arr)
-        
-        for i in range(len(arr) - 2, -1, -1):
+        n = len(arr)
+        for i in range(n-2, -1, -1):
             if arr[i] > arr[i + 1]:
-                ind = i
-                break
-        if ind == len(arr):
-            return arr
-        
-        change = ind + 1
-        prev = arr[ind + 1]
-        for i in range(ind + 1, len(arr)):
-            if arr[i] > prev and arr[i] < arr[ind]:
-                change = i
-                
-        arr[ind], arr[change] = arr[change], arr[ind]
-                
+                change = bisect.bisect_left(arr, arr[i], i+1, n)
+                target = bisect.bisect_left(arr, arr[change-1], i+1, change)
+                arr[i], arr[target] = arr[target], arr[i]
+
+                return arr
+            
         return arr
