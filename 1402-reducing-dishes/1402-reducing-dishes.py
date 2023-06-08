66ac401099c472986039512a2bca9f4b703fcfbd
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-06-08 23:19:02 +0300

    Time: 46 ms (85.84%), Space: 16.4 MB (26.20%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,12 +1,21 @@
 class Solution:
-    def maxSatisfaction(self, satisfaction: List[int]) -> int:
-        arr, max_val = deque(sorted(satisfaction)), 0
+    def maxSatisfaction(self, s: List[int]) -> int:
+        s.sort()
+        prev_product, prev_sum =  0, 0
+        n , ind= len(s), 1
         
-        def calc(arr):
-            return sum((i+1)*val for i, val in enumerate(arr))
-        
-        while arr:
-            max_val = max(max_val, calc(arr))
-            arr.popleft()
+        for i in range(n):
+            prev_product += (i + 1) * s[i]
+            prev_sum += s[i]
             
-        return max_val
+        ans = max(0, prev_product)
+        while ind < n:
+            prev_sum -= s[ind - 1]
+            prev_product -= (s[ind - 1] + (prev_sum))
+            
+            ans = max(ans, prev_product)
+            ind += 1
+            
+        return ans
+            
+            
