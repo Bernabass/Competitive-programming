b2d3d88e26c4c5bf71353d2ea9a950ff403a225d
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-07-11 22:07:21 +0300

    Time: 1470 ms (21.44%), Space: 130.4 MB (5.01%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -2,27 +2,27 @@ class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         ans = []
         n = len(nums)
+        nums.sort()
         
         for i in range(n):
             for j in range(i+1, n):
                 x = target - nums[i] - nums[j]
-                pair = self.two_sum(nums[j+1:], x)
+                pair = self.two_sum(nums, x, j+1)
                 
                 if pair:
                     for val in pair:
-                        ans.append(tuple(sorted((nums[i], nums[j]) + val)))
+                        ans.append((nums[i], nums[j]) + val)
                     
         return set(ans)
                     
-    def two_sum(self, arr, target):
-        info = {}
-        ans = []
-        for idx, val in enumerate(arr):
+    def two_sum(self, arr, target, idx):
+        ans, seen = [], set()
+        for i in range(idx, len(arr)):
+            val = arr[i]
             x = target - val
-            if x in info:
-                ans.append((val, x))
-            
-            info[val] = idx
+            if x in seen:
+                ans.append((x, val))
+                
+            seen.add(val)
                 
         return ans
-                
