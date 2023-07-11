a5091fe9b7e0c55a3afbd58b2d2effd66929ce06
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-07-11 23:03:30 +0300

    Time: 815 ms (67.67%), Space: 16.7 MB (9.19%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,24 +1,42 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        ans, n = [], len(nums)
+        ans, prev = [], []
+        n = len(nums)
         nums.sort()
         
-        for i in range(n):
-            for j in range(i+1, n):
-                x = target - nums[i] - nums[j]
-                left, right = j+1, n - 1
-                
+        def back_track(idx, target, k):
+            if k == 2:
+                left, right = idx,  n - 1
+
                 while left < right:
-                    
-                    if nums[left] + nums[right] < x:
+
+                    if nums[left] + nums[right] < target:
                         left += 1
-                        
-                    elif nums[left] + nums[right] > x:
+
+                    elif nums[left] + nums[right] > target:
                         right -= 1
-                        
+
                     else:
-                        ans.append((nums[i], nums[j], nums[left], nums[right]))
+                        ans.append(prev + [nums[left], nums[right]])
                         left += 1
                         right -= 1
                         
-        return set(ans)
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                            
+                        while right > left and nums[right] == nums[right + 1]:
+                            right -= 1
+                            
+                return
+            
+            for i in range(idx, n):
+                if i > idx and nums[i] == nums[i-1]:
+                    continue
+                    
+                prev.append(nums[i])
+                back_track(i+1, target - nums[i], k - 1)
+                prev.pop()
+    
+        back_track(0, target, 4)
+        
+        return ans
