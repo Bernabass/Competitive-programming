1ad4d8dea9ec20a0747cbb11062253b0320afa47
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-07-11 23:00:15 +0300

    Time: 1186 ms (35.68%), Space: 74.1 MB (5.01%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,37 +1,24 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        ans, prev = [], []
-        n = len(nums)
+        ans, n = [], len(nums)
         nums.sort()
         
-        def back_track(idx, target, k):
-            if k == 2:
-                right = n - 1
-
-                while idx < right:
-
-                    if nums[idx] + nums[right] < target:
-                        idx += 1
-
-                    elif nums[idx] + nums[right] > target:
-                        right -= 1
-
-                    else:
-                        ans.append(prev + [nums[idx], nums[right]])
-                        idx += 1
-                        while idx < right and nums[idx] == nums[idx-1]:
-                            idx += 1
-                            
-                return
-            
-            for i in range(idx, n):
-                if i > idx and nums[i] == nums[i-1]:
-                    continue
+        for i in range(n):
+            for j in range(i+1, n):
+                x = target - nums[i] - nums[j]
+                left, right = j+1, n - 1
+                
+                while left < right:
                     
-                prev.append(nums[i])
-                back_track(i+1, target - nums[i], k - 1)
-                prev.pop()
-    
-        back_track(0, target, 4)
-        
-        return ans
+                    if nums[left] + nums[right] < x:
+                        left += 1
+                        
+                    elif nums[left] + nums[right] > x:
+                        right -= 1
+                        
+                    else:
+                        ans.append((nums[i], nums[j], nums[left], nums[right]))
+                        left += 1
+                        right -= 1
+                        
+        return set(ans)
