f5fdabc6aff18998fdaef0920f45f87b46f14636
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-07-13 08:30:14 +0300

    Time: 58 ms (49.03%), Space: 16.6 MB (21.07%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -1,8 +1,11 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
         
-        @cache
+        memo = {}
         def back_track(target):
+            if target in memo:
+                return memo[target]
+            
             if target < 0:
                 return 0
             
@@ -12,6 +15,9 @@ class Solution:
             count = 0
             for num in nums:
                 count += back_track(target - num)
+                
+                
+            memo[target] = count
                 
             return count
         
