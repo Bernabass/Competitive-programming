ffcc88c933cc354577933ff0f895e1f1417498f0
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-07-13 17:12:41 +0300

    Time: 1394 ms (26.46%), Space: 16.4 MB (41.59%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,42 +1,29 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        ans, prev = [], []
-        n = len(nums)
+        ans = set()
+
         nums.sort()
-        
-        def back_track(idx, target, k):
-            if k == 2:
-                left, right = idx,  n - 1
+        n = len(nums)
 
-                while left < right:
+        def two_sum(idx, target):
+            ans = []
+            info = {}
+            for i in range(idx, len(nums)):
+                val = nums[i]
+                if target - val in info:
+                    ans.append([info[target - val], i])
 
-                    if nums[left] + nums[right] < target:
-                        left += 1
+                info[val] = i
 
-                    elif nums[left] + nums[right] > target:
-                        right -= 1
+            return ans
 
-                    else:
-                        ans.append(prev + [nums[left], nums[right]])
-                        left += 1
-                        right -= 1
-                        
-                        while left < right and nums[left] == nums[left - 1]:
-                            left += 1
-                            
-                        while right > left and nums[right] == nums[right + 1]:
-                            right -= 1
-                            
-                return
-            
-            for i in range(idx, n):
-                if i > idx and nums[i] == nums[i-1]:
-                    continue
-                    
-                prev.append(nums[i])
-                back_track(i+1, target - nums[i], k - 1)
-                prev.pop()
-    
-        back_track(0, target, 4)
-        
+        for i in range(n):
+            for j in range(i+1, n):
+                s = target - (nums[i] + nums[j])
+                pair = two_sum(j+1, s)
+
+                if pair:
+                    for k, l in pair:
+                        ans.add((nums[i], nums[j], nums[k], nums[l]))
+
         return ans
