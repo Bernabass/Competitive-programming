ba7992042922cc27cc854d1955359178e78cc0f1
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-07-13 08:33:10 +0300

    Time: 60 ms (38.18%), Space: 16.2 MB (98.23%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -1,24 +1,11 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        
-        memo = {}
-        def back_track(target):
-            if target in memo:
-                return memo[target]
-            
-            if target < 0:
-                return 0
-            
-            if target == 0:
-                return 1
-            
-            count = 0
+        dp = [0]*(target + 1)
+        dp[0] = 1
+
+        for i in range(1, target+1):
             for num in nums:
-                count += back_track(target - num)
-                
-                
-            memo[target] = count
-                
-            return count
-        
-        return back_track(target)
+                if i - num >= 0:
+                    dp[i] += dp[i - num]
+
+        return dp[target]
