cf985cdb0cdf2aa424e9890b56d6f23ab0adbdc8
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-07-16 15:23:35 +0300

    Time: 68 ms (26.72%), Space: 20.7 MB (32.07%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -2,17 +2,20 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
         @cache
-        def back_track(idx, do, cooldown):
-            if idx == n:
+        def back_track(idx, hold):
+            if idx >= n:
                 return 0
             
-            if not cooldown:
-                curr1 = do*prices[idx] + back_track(idx+1, -do, not(do < 0))
-                    
-                curr2 = back_track(idx+1, do, cooldown)
+            if hold:
+                sell_profit = prices[idx] + back_track(idx + 2, 0)
+                hold_profit = back_track(idx + 1, 1)
                 
-                return max(curr1, curr2)
+                return max(sell_profit, hold_profit)
             
-            return back_track(idx+1, do, 1-cooldown)
+            else:
+                buy_profit = -prices[idx] + back_track(idx + 1, 1)
+                hold_profit = back_track(idx + 1, 0)
+                
+                return max(buy_profit, hold_profit)
         
-        return back_track(0, -1, 0)
+        return back_track(0, 0)
