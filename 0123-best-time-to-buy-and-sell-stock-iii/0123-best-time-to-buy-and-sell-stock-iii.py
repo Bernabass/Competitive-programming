2ef218027b66bacc34f20c4ccf1675909a727af4
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-07-16 08:00:38 +0300

    Time: 4856 ms (5.02%), Space: 549.8 MB (6.28%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,12 +1,21 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        
-        @cache
-        def back_track(idx, do, count):
-            if idx == n or count == 4:
+        memo = {}
+
+        def back_track(idx, state):
+            if idx == n or state == 4:
                 return 0
-            
-            return max(do*prices[idx] + back_track(idx+1, -do, count+1), back_track(idx+1, do, count))
-        
-        return back_track(0, -1, 0)
+
+            if (idx, state) in memo:
+                return memo[(idx, state)]
+
+            do = -1 if state % 2 == 0 else 1
+            max_profit = max(
+                do * prices[idx] + back_track(idx + 1, state + 1),
+                back_track(idx + 1, state)
+            )
+            memo[(idx, state)] = max_profit
+            return max_profit
+
+        return back_track(0, 0)
