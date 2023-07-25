93bb32402cd055c9492ecd442a222a530014a4d4
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-07-25 19:42:33 +0300

    Time: 836 ms (44.82%), Space: 16.5 MB (27.18%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -4,26 +4,28 @@ class Solution:
         n = len(s)
         
         def check(idx):
-            curr_len1 = curr_len2 = s[idx]       
+            curr_len1 = deque(s[idx])
+            curr_len2 = deque(s[idx])
 
             i, j = idx - 1, idx + 1
             k, l = idx - 1, idx + 2
             
             while i >= 0 and j < len(s):
                 if s[i] == s[j]:
-                    curr_len1 = s[i] + curr_len1 + s[j]
+                    curr_len1.appendleft(s[i])
+                    curr_len1.append(s[j])
                     i -= 1
                     j += 1
-
-                    
+   
                 else:
                     break
              
             if len(set(s[idx:idx+2])) == 1:
-                curr_len2 = s[idx:idx+2]
+                curr_len2 = deque(s[idx:idx+2])
                 while k >= 0 and l < len(s):
                     if s[k] == s[l]:
-                        curr_len2 = s[k] + curr_len2 + s[l]
+                        curr_len2.appendleft(s[k])
+                        curr_len2.append(s[l])
                         k -= 1
                         l += 1
 
@@ -40,4 +42,4 @@ class Solution:
             if len(curr) > len(max_len):
                 max_len = curr
             
-        return max_len
+        return "".join(max_len)
