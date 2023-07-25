70805e2e374613a2c4e491f80e63218e0e1b1c3c
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-07-25 19:58:15 +0300

    Time: 469 ms (90.63%), Space: 16.5 MB (47.74%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,45 +1,27 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        max_len = s[0]
         n = len(s)
-        
-        def check(idx):
-            curr_len1 = deque(s[idx])
-            curr_len2 = deque(s[idx])
-
-            i, j = idx - 1, idx + 1
-            k, l = idx - 1, idx + 2
-            
-            while i >= 0 and j < len(s):
-                if s[i] == s[j]:
-                    curr_len1.appendleft(s[i])
-                    curr_len1.append(s[j])
-                    i -= 1
-                    j += 1
-   
-                else:
-                    break
-             
-            if len(set(s[idx:idx+2])) == 1:
-                curr_len2 = deque(s[idx:idx+2])
-                while k >= 0 and l < len(s):
-                    if s[k] == s[l]:
-                        curr_len2.appendleft(s[k])
-                        curr_len2.append(s[l])
-                        k -= 1
-                        l += 1
-
-                    else:
-                        break
+        max_sub = s[0]
                     
-            if len(curr_len1) > len(curr_len2):
-                return curr_len1
-              
-            return curr_len2
-            
+        def build(left, right):
+            while left >= 0 and right < n and s[left] == s[right]:
+                left -= 1
+
+                right += 1
+
+            return s[left + 1:right]
+        
         for idx in range(n):
-            curr = check(idx)
-            if len(curr) > len(max_len):
-                max_len = curr
+            pal_1 = build(idx, idx)
+            pal_2 = build(idx, idx + 1)
             
-        return "".join(max_len)
+            if len(pal_1) > len(pal_2):
+                curr_max = pal_1
+                
+            else:
+                curr_max = pal_2
+            
+            if len(curr_max) > len(max_sub):
+                max_sub = curr_max
+
+        return max_sub
