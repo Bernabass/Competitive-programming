54fbf84904378f505ca4493a2bc3fae80a5c6b2b
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-08-01 12:26:14 +0300

    Time: 771 ms (47.85%), Space: 221.3 MB (15.35%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
@@ -1,18 +1,33 @@
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
-        n=len(s1)
-        m=len(s2)
-        dp=[[0 for x in range(m+1)] for x in range(n+1)]
-        for i in range(1,n+1):
-            dp[i][0]=dp[i-1][0]+ord(s1[i-1])
+        n1, n2 = len(s1), len(s2)
+        sum1 = self.suffix_sum(s1) + [0]
+        sum2 = self.suffix_sum(s2) + [0]
 
-        for i in range(1,m+1):
-            dp[0][i]=dp[0][i-1]+ord(s2[i-1])
-        for i in range(1,n+1):
-            for j in range(1,m+1):
-                if s1[i-1]==s2[j-1]:
-                    dp[i][j]=dp[i-1][j-1]
-                else:
-                    dp[i][j]=min(dp[i-1][j]+ord(s1[i-1]),dp[i][j-1]+ord(s2[j-1]))
-
-        return dp[n][m]  
+        @cache
+        def dp(i, j):
+            if i == n1:
+                return sum2[j]
+            
+            if j == n2:
+                return sum1[i]
+            
+            
+            if s1[i] == s2[j]:
+                return dp(i + 1, j + 1)
+            
+            else:
+                return min(ord(s1[i]) + dp(i + 1, j), ord(s2[j]) + dp(i, j + 1))
+            
+            
+        return dp(0, 0)
+            
+               
+            
+    def suffix_sum(self, string):
+        result = [0] * len(string)
+        result[-1] = ord(string[-1])
+        for i in range(len(string)-2, -1, -1):
+            result[i] = result[i+1] + ord(string[i])
+            
+        return result
