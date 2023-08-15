70dc8f0fd73277eb75d6a0c95bc9218153f8b797
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-08-15 11:49:22 +0300

    Time: 46 ms (89.53%), Space: 16.6 MB (27.21%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -13,23 +13,21 @@ class Solution:
             if  i < ns and (s[i] == p[j] or p[j] == ".") and dp(i + 1, j + 1):
                 return True
             
-            
             if j + 1 < np and p[j + 1] == "*":
                 if dp(i, j + 2):
                     return True
                 
                 if p[j].isalpha():
-                    for idx in range(i, ns):
+                    idx = i
+                    while idx < ns and s[idx] == p[j]:
                         if s[idx] == p[j]:
                             if dp(idx + 1, j + 2):
                                 return True
-
-                        else:
-                            break
+                        idx += 1
                     
                 else:
                     for idx in range(i, ns):
                         if dp(idx + 1, j + 2):
                             return True
                     
-        return dp(0, 0)        
+        return dp(0, 0)
