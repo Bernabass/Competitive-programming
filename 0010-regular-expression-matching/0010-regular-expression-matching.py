b00580f32c832d9bff499899e2d86e1c4e6e2ef8
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-08-15 12:00:32 +0300

    Time: 53 ms (72.75%), Space: 16.6 MB (27.21%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,33 +1,17 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        ns, np = len(s), len(p)
-        
         @cache
         def dp(i, j):
-            if i == ns and j == np:
+            if i == len(s) and j == len(p):
                 return True
-            
-            if j >= np:
+
+            if j >= len(p):
                 return False
-            
-            if  i < ns and (s[i] == p[j] or p[j] == ".") and dp(i + 1, j + 1):
-                return True
-            
-            if j + 1 < np and p[j + 1] == "*":
-                if dp(i, j + 2):
-                    return True
-                
-                if p[j].isalpha():
-                    idx = i
-                    while idx < ns and s[idx] == p[j]:
-                        if s[idx] == p[j]:
-                            if dp(idx + 1, j + 2):
-                                return True
-                        idx += 1
-                    
-                else:
-                    for idx in range(i, ns):
-                        if dp(idx + 1, j + 2):
-                            return True
-                    
+
+            match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+            if j + 1 < len(p) and p[j + 1] == "*":
+                return (dp(i, j + 2) or (match and dp(i + 1, j)))
+
+            return match and dp(i + 1, j + 1)
+
         return dp(0, 0)
