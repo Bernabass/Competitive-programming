aafb73d0ff751b4e38540234bdfe3446503408bc
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-08-18 19:35:46 +0300

    Time: 2949 ms (46.25%), Space: 97 MB (51.47%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
+        n = len(stoneValue)
+        @cache
+        def dp(i):
+            possible_ans = []
+            if i < n:
+                first_1, second_1 = dp(i + 1)
+                first_ans_1, second_ans_1 = stoneValue[i] + second_1, first_1
+                possible_ans.append((first_ans_1 - second_ans_1, (first_ans_1, second_ans_1)))
+            if i + 1 < n:
+                first_2, second_2 = dp(i + 2)
+                first_ans_2, second_ans_2 = stoneValue[i] + stoneValue[i + 1] + second_2, first_2
+                possible_ans.append((first_ans_2 - second_ans_2, (first_ans_2, second_ans_2)))
+            if i + 2 < n:
+                first_3, second_3 = dp(i + 3)
+                first_ans_3, second_ans_3 = stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] + second_3, first_3
+                possible_ans.append((first_ans_3 - second_ans_3, (first_ans_3, second_ans_3)))
+            
+            possible_ans.sort()
+            
+            return possible_ans[-1][1] if possible_ans else [0, 0]
+        
+        
+        alice, bob = dp(0)
+        
+        if bob > alice:
+            return "Bob"
+        
+        elif bob < alice:
+            return "Alice"
+        
+        return "Tie"
