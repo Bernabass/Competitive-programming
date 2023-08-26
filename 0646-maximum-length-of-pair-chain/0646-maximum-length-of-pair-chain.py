65754f0cb0a5471f0f25fcf86ffb4a793368070e
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-08-26 17:08:57 +0300

    Time: 2294 ms (10.52%), Space: 16.9 MB (35.70%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.py
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.py
@@ -5,11 +5,10 @@ class Solution:
         @cache
         def dp(idx, prev):            
             max_len = 0
-            for i in range(idx, len(pairs)):
+            for i in range(len(pairs)):
                 if pairs[i][0] > prev:
-                    max_len = max(max_len, 1 + dp(i + 1, pairs[i][1]))
+                    max_len = max(max_len, dp(i + 1, pairs[i][1]) + 1)
                     
             return max_len
-        
-        return dp(0, float("-inf"))
-        
+                    
+        return dp(0, -inf)
