c9bbf2d24e70342395b500617455c1cbb0e2b37b
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-08-29 16:03:10 +0300

    Time: 66 ms (50.20%), Space: 16.4 MB (51.00%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.py
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.py
@@ -1,34 +1,30 @@
 class Solution:
     def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
-        
-        def CanBeFormed(word, collection):
+
+        def valid(word, freq):
+            used = defaultdict(int)
             for char in word:
-                if collection[char] <= 0:
+                if freq[char] - used[char] <= 0:
                     return False
-                collection[char] -= 1
                 
+                used[char] += 1
+            
             return True
         
-        def total(word):
-            total_score = 0
-            
-            for char in word:
-                total_score += score[ord(char) - ord("a")]
-                
-            return total_score
+        def TotalScore(word):
+            return sum(score[ord(char) - ord("a")] for char in word)
         
-        
-        def dp(rem_letters, idx):
+        def back_track(idx, freq):
             if idx == len(words):
                 return 0
             
             take = leave = 0
             curr_word = words[idx]
-            if CanBeFormed(curr_word, deepcopy(rem_letters)):
-                take = total(curr_word) + dp(rem_letters - Counter(curr_word), idx + 1)
+            if valid(curr_word, freq):
+                take = TotalScore(curr_word) + back_track(idx + 1, freq - Counter(curr_word))
                 
-            leave = dp(rem_letters, idx + 1)
+            leave = back_track(idx + 1, freq)
             
             return max(take, leave)
         
-        return dp(Counter(letters), 0)
+        return back_track(0, Counter(letters))
