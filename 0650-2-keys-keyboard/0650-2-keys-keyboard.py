3ef2ef53b4f79aba9df3a5a945747cdf05c02d36
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-09-18 10:31:44 +0300

    Time: 159 ms (48.05%), Space: 38.7 MB (7.81%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,25 +1,21 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        if n == 1:
-            return 0
-
-        level, seen = [(0, 1)], {(0, 1)}
-        depth = 0
-
-        while level:
-            depth += 1
-            next_level = []
+        
+        @cache
+        def dp(written, copied):
+            paste = copy = inf
+            if written == n:
+                return 0
             
-            for copy, paste in level:
-                if (copy+paste, copy) not in seen and paste+copy <= 1000:
-                    if copy+paste == n:
-                        return depth
-                    
-                    next_level.append((copy, copy+paste))
-                    seen.add((copy, copy+paste))
-                    
-                if (paste, paste) not in seen and paste <= 1000:
-                    next_level.append((paste, paste))
-                    seen.add((paste, paste))
-                    
-            level = next_level
+            if written > n:
+                return paste
+            
+            if copied:
+                paste = dp(written + copied, copied)
+                
+            if written > copied:
+                copy = dp(written, written)
+                
+            return min(copy, paste) + 1
+        
+        return dp(1, 0)
