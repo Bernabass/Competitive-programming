b61b79ea094b73c45e988f353b3edf90eb86f68d
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-09-18 11:12:14 +0300

    Time: 76 ms (46.88%), Space: 19.7 MB (10.85%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.py
+++ 0174-dungeon-game/0174-dungeon-game.py
@@ -5,14 +5,14 @@ class Solution:
         @cache
         def dp(i, j):
             if i == m or j == n:
-                return float("inf")
+                return inf
             
             if i == m - 1 and j == n - 1:
                 return max(1, 1 - dungeon[i][j])
             
-            right = dp(i, j + 1)
-            down = dp(i + 1, j)
+            right = max(1, dp(i, j + 1) - dungeon[i][j])
+            down = max(1, dp(i + 1, j) - dungeon[i][j])
             
-            return max(1, min(right, down) - dungeon[i][j])
+            return min(right, down)
         
         return dp(0, 0)
