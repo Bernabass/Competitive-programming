1ad3609a0381d5a903d601ff9e1f7d60354ede52
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-09-26 11:49:35 +0300

    Time: 137 ms (36.69%), Space: 16.7 MB (85.96%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -1,61 +1,26 @@
-class TrieNode:
-    
-    def __init__(self):
-        self.children = {}
-        self.is_end = False
-        
-class Trie:
-    
-    def __init__(self):
-        self.root = TrieNode()
-        
-    def insert(self, word: str) -> None:
-        node = self.root
-        
-        for char in word:
-            if char not in node.children:
-                node.children[char] = TrieNode()
-                
-            node = node.children[char]
-            
-        node.is_end = True
-            
-    def search(self, word: str) -> bool:
-        node = self.root
-        
-        for idx, char in enumerate(word):
-            if idx and not node.is_end or char not in node.children:
-                return False
-            
-            node = node.children[char]
-            
-        return node.is_end
-    
-    def startsWith(self, prefix: str) -> bool:
-        node = self.root
-        
-        for char in prefix:
-            if char not in node.children:
-                return False
-            
-            node = node.children[char]
-            
-        return True
-
 class Solution:
     def longestWord(self, words: List[str]) -> str:
-        trie = Trie()
+        words.sort()
+        prev = set()
         longest_word = ""
         
-        for word in words:
-            trie.insert(word)
+        for i in range(len(words)):
+            word = words[i]
+            curr = ""
             
-        for word in words:
-            if trie.search(word):
+            for j in range(len(word) - 1):
+                char = word[j]
+                curr += char
+                if curr not in prev:
+                    break
+                    
+            else:
                 if len(word) > len(longest_word):
                     longest_word = word
                     
                 elif len(word) == len(longest_word):
                     longest_word = min(longest_word, word)
                     
-        return longest_word       
+            prev.add(word)
+                    
+        return longest_word
