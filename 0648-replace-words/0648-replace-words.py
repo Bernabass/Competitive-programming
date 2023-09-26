316306fe4aeb4c6680748caa20628eb18343d8e5
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-09-26 18:13:58 +0300

    Time: 111 ms (83.80%), Space: 38.1 MB (33.65%) - LeetHub

--- 0648-replace-words/0648-replace-words.py
+++ 0648-replace-words/0648-replace-words.py
@@ -1,16 +1,52 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = -1
+        
+class Trie:
+    
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def insert(self, word, index) -> None:
+        node = self.root
+        
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+                
+            node = node.children[char]
+            
+        node.is_end = index
+            
+    def search(self, word):
+        node = self.root
+        
+        for char in word:
+            if node.is_end != -1:
+                return node.is_end
+            
+            if char not in node.children:
+                return -1
+            
+            node = node.children[char]
+            
+        return -1
+    
+
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        info = set(dictionary)
-        arr = sentence.split()
+        trie = Trie()
+        res = sentence.split()
         
-        for idx, word in enumerate(arr):
-            curr = ""
+        for idx, word in enumerate(dictionary):
+            trie.insert(word, idx)
             
-            for char in word:
-                curr += char
+        for idx, word in enumerate(res):
+            val = trie.search(word)
+            
+            if val != -1:
+                res[idx] = dictionary[val]
                 
-                if curr in info:
-                    arr[idx] = curr
-                    break
-                    
-        return " ".join(arr)
+        return " ".join(res)
+                
