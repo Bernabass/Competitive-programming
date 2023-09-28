23523d04f18d864d827a9fd2dc39b256644d3e86
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-09-28 21:50:12 +0300

    Time: 397 ms (25.40%), Space: 31.4 MB (56.35%) - LeetHub

--- 2568-minimum-impossible-or/2568-minimum-impossible-or.py
+++ 2568-minimum-impossible-or/2568-minimum-impossible-or.py
@@ -1,8 +1,7 @@
 class Solution:
     def minImpossibleOR(self, nums: List[int]) -> int:
         nums = set(nums)
-        pos_ans = 2
-        
         for i in range(33):
-            if (pos_ans ** i) not in nums:
-                return pos_ans ** i
+            ans = 1 << i
+            if ans not in nums:
+                return ans
