893777c7cd64709ee133f730900ad7f665fa981b
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-10-06 16:14:00 +0300

    Time: 28 ms (98.38%), Space: 16 MB (97.72%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -1,18 +1,12 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
         
-        @cache
-        def dp(Sum):
-            if Sum == n:                    
-                return 1
-                    
-            if Sum > n:
-                return 0
-            
-            max_product = 1
-            for num in range(n - 1, 0, -1):
-                max_product = max(max_product, num *dp(Sum + num))
-                
-            return max_product
+        if n <= 3:
+            return n - 1
         
-        return dp(0)
+        ans = 1
+        while n > 4:
+            ans *= 3
+            n -= 3
+            
+        return ans * n
