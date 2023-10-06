451ee5dc9dfcc0c8fb00f35964720d8c8ae86c5b
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-10-06 10:15:30 +0300

    Time: 688 ms (67.99%), Space: 43.7 MB (67.35%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.py
+++ 1146-snapshot-array/1146-snapshot-array.py
@@ -1,27 +1,30 @@
 class SnapshotArray:
     def __init__(self, length: int):
-        self.map = defaultdict(list)
+        self.arr = defaultdict(list)
         self.snapId = 0
 
-    def set(self, index: int, val: int) -> None:
-        if self.map[index] and self.map[index][-1][0] == self.snapId:
-            self.map[index][-1][1] = val
-            return
-        self.map[index].append([self.snapId, val])
+    def set(self, index: int, val: int) -> None:        
+        self.arr[index].append([self.snapId, val])
 
     def snap(self) -> int:
         self.snapId += 1
+                    
         return self.snapId - 1
 
     def get(self, index: int, snap_id: int) -> int:
-        arr = self.map[index]
+        arr = self.arr[index]
         left, right, ans = 0, len(arr) - 1, -1
+        
         while left <= right:
             mid = (left + right) // 2
             if arr[mid][0] <= snap_id:
                 ans = mid
                 left = mid + 1
+                
             else:
                 right = mid - 1
-        if ans == -1: return 0
+                
+        if ans == -1:
+            return 0
+        
         return arr[ans][1]
