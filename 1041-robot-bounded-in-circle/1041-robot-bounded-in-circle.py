68a115eb903f66c8cd4e47de9c99d2cc0d00783a
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-10-11 17:12:19 +0300

    Time: 1469 ms (5.20%), Space: 16.2 MB (56.35%) - LeetHub

---
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+        
+        def right(x, y):
+            return [y, -x]
+        
+        def left(x, y):
+            return [-y, x]
+        
+        direction = [1, 0]
+        coordinate = [0, 0]
+        
+        for _ in range(10000):
+        
+            for char in instructions:
+                if char == "G":
+                    coordinate[0] += direction[0]
+                    coordinate[1] += direction[1]
+
+                elif char == "R":
+                    coordinate = right(coordinate[0], coordinate[1])
+
+                else:
+                    coordinate = left(coordinate[0], coordinate[1])
+                    
+            if coordinate == [0, 0]:
+                return True
