c0a6ff4c902d8bffab0a2186fb29222a8d581f26
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-10-12 11:19:18 +0300

    Time: 38 ms (63.57%), Space: 17 MB (90.39%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.py
@@ -0,0 +1,75 @@
+# """
+# This is MountainArray's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class MountainArray:
+#    def get(self, index: int) -> int:
+#    def length(self) -> int:
+
+class Solution:
+    def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
+        N = mountain_arr.length()
+        left, right = 0, N - 1
+        
+        @cache
+        def get(idx):
+            return mountain_arr.get(idx)
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if get(mid - 1) > get(mid) > get(mid + 1):
+                right = mid
+
+            elif get(mid - 1) < get(mid) < get(mid + 1):
+                left = mid
+
+            else:
+                peak = mid
+                break
+    
+    
+        left, right = 0, peak
+        ans = -1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if get(mid) > target:
+                right = mid - 1
+
+            elif get(mid) < target:
+                left = mid + 1
+
+            else:
+                if mid - 1 > -1 and get(mid - 1) == target:
+                    right = mid - 1
+
+                else:
+                    ans = mid
+                    break
+
+        if ans != -1:
+            return ans
+
+        left, right = N - 1, peak + 1
+        ans = -1
+
+        while left >= right:
+            mid = (left + right) // 2
+
+            if get(mid) > target:
+                right = mid + 1
+
+            elif get(mid) < target:
+                left = mid - 1
+
+            else:
+                if mid + 1 < N and get(mid + 1) == target:
+                    right = mid + 1
+
+                else:
+                    ans = mid
+                    break
+
+        return ans
