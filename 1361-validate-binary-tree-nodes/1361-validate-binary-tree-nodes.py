10238f442cfe9f73af69a333d5baa37c30c525bf
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-10-17 23:24:00 +0300

    Time: 312 ms (20.97%), Space: 31.2 MB (5.17%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
@@ -1,7 +1,7 @@
 class Solution:
     def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
         GRAPH = defaultdict(list)
-        # seen = set()
+        memo = defaultdict(int)
         max_count = 0
         for node in range(n):
             if leftChild[node] != -1:
@@ -11,6 +11,8 @@ class Solution:
           
         
         def dfs(node, seen):
+            if node in memo:
+                return memo[node]
             seen.add(node)
             count = 1
             for adj in GRAPH[node]:
@@ -23,13 +25,15 @@ class Solution:
                 
                 count += ans
                 
+            memo[node] = count
+                
             return count
         
         for node in range(n):
             ans = dfs(node, set())
             if ans == -1:
                 return False
-
+            
             max_count = max(ans, max_count)
             
         return max_count == n
