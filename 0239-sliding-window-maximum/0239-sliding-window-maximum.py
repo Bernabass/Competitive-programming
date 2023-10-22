1fbc26d654c792df44d48f0a1877f0c56a47fa92
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-10-22 11:40:47 +0300

    Time: 1338 ms (56.07%), Space: 33.1 MB (73.26%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,48 +1,20 @@
-class SegmentTree:
-    def __init__(self, arr):
-        self.size = len(arr)
-        self.tree = [0]*self.size + arr
- 
-        for i in range(self.size-1, 0, -1):
-            self.tree[i] = self._combine(self.tree[2*i], self.tree[2*i + 1])
-
-    def _combine(self, a, b):
-        return max(a, b)  
-
-    def update(self, index, value):
-        index += self.size
-        self.tree[index] += value
-        while index > 1:
-            index //= 2
-            self.tree[index] = self._combine(self.tree[2*index], self.tree[2*index + 1])
-
-    def query(self, left, right):
-        left += self.size
-        right += self.size
-        result = self._neutral_element()
-
-        while left <= right:
-            if left % 2 == 1:
-                result = self._combine(result, self.tree[left])
-                left += 1
-            if right % 2 == 0:
-                result = self._combine(result, self.tree[right])
-                right -= 1
-
-            left //= 2
-            right //= 2
-
-        return result
-
-    def _neutral_element(self):
-        return -float("inf")
-
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        s_tree = SegmentTree(nums)
+        N = len(nums)
+        queue = deque()
         ans = []
-        
-        for idx in range(len(nums) - k + 1):
-            ans.append(s_tree.query(idx, idx + k - 1))
+
+        for right in range(N):
             
+            while queue and queue[0] < right - k + 1:
+                queue.popleft()
+
+            while queue and nums[right] >= nums[queue[-1]]:
+                queue.pop()
+
+            queue.append(right)
+
+            if right >= k - 1:
+                ans.append(nums[queue[0]])
+
         return ans
