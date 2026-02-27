e17e0dc748eb361f380c31bdf9f41a4f51677817
Bernabass <99589215+Bernabass@users.noreply.github.com>
2026-02-27 13:20:56 +0300

    Add solution for Palindrome Number

--- leetcode/0009-palindrome-number/0009-palindrome-number.py
+++ leetcode/0009-palindrome-number/0009-palindrome-number.py
@@ -1,12 +1,1 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        
-        if x < 0 or (x and not (x % 10)):
-            return False
-
-        first_half = 0
-        while x > first_half:
-            first_half = first_half * 10 + (x % 10)
-            x //= 10
-
-        return first_half == x or  (first_half // 10) == x
+another check
